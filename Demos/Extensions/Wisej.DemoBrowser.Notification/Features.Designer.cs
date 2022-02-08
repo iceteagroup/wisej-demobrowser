258d1848cf43f2b9a220a17bae7c7d69bb8ff6f4
LRufenacht <llrufenacht@gmail.com>
2022-02-08 16:48:28 -0500

    Notification.

---
+++ Demos/Extensions/Wisej.DemoBrowser.Notification/Features.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Wisej.DemoBrowser.Notification
+{
+	partial class Features
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notification1 = new Wisej.Web.Ext.Notification.Notification(this.components);
+			this.buttonProgress = new Wisej.Web.Button();
+			this.SuspendLayout();
+			// 
+			// notification1
+			// 
+			this.notification1.Click += new Wisej.Web.Ext.Notification.NotificationClickEventHandler(this.notification1_Click);
+			// 
+			// buttonProgress
+			// 
+			this.buttonProgress.Anchor = Wisej.Web.AnchorStyles.None;
+			this.buttonProgress.Location = new System.Drawing.Point(618, 219);
+			this.buttonProgress.Name = "buttonProgress";
+			this.buttonProgress.Size = new System.Drawing.Size(186, 37);
+			this.buttonProgress.TabIndex = 0;
+			this.buttonProgress.Text = "Start Task";
+			this.buttonProgress.Click += new System.EventHandler(this.buttonProgress_Click);
+			// 
+			// Features
+			// 
+			this.Controls.Add(this.buttonProgress);
+			this.Name = "Features";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Web.Ext.Notification.Notification notification1;
+		private Web.Button buttonProgress;
+	}
+}
---
+++ Demos/Extensions/Wisej.DemoBrowser.Notification/Features.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Wisej.DemoBrowser.Common;
+using Wisej.Web;
+
+namespace Wisej.DemoBrowser.Notification
+{
+	public partial class Features : DemoView
+	{
+		public Features()
+		{
+			InitializeComponent();
+		}
+
+		private void notification1_Click(object sender, Web.Ext.Notification.NotificationClickEventArgs e)
+		{
+			AlertBox.Show($"Clicked {e.Title}");
+		}
+
+		private void buttonProgress_Click(object sender, EventArgs e)
+		{
+			this.buttonProgress.Enabled = false;
+
+			Application.StartTask(() =>
+			{
+				for (int i = 0; i<=4; i++)
+				{
+					Thread.Sleep(5000);
+					
+					this.notification1.Show("Progress Update", $"Progress: {i * 25}%", icon: "icon-info", true);
+					this.buttonProgress.Text = $"Progress: {i * 25}%";
+
+					Application.Update(this);
+				}
+
+				this.buttonProgress.Text = "Start Task";
+				this.buttonProgress.Enabled = true;
+				
+				Application.Update(this);
+			});
+		}
+	}
+}
