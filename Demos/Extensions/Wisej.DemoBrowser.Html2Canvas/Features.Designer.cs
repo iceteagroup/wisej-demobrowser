22bbd03a4163e383c880cec2bcd12ce1ab0e4946
Alaa Trigui <alaa.trigui@outlook.com>
2022-03-07 14:39:34 +0100

    Added Html2Canvas demo

---
+++ Demos/Extensions/Wisej.DemoBrowser.Html2Canvas/Features.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Wisej.DemoBrowser.Html2Canvas
+{
+    partial class Features
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Features));
+            this.flowLayoutPanel1 = new Wisej.Web.FlowLayoutPanel();
+            this.btnCaptureChart = new Wisej.Web.Button();
+            this.btnHtmlPanel = new Wisej.Web.Button();
+            this.btnCapturePicBox = new Wisej.Web.Button();
+            this.btnCapturePanel = new Wisej.Web.Button();
+            this.btnCaptureDemo = new Wisej.Web.Button();
+            this.btnCaptureWindow = new Wisej.Web.Button();
+            this.pnlDemo = new Wisej.Web.Panel();
+            this.pictureBox = new Wisej.Web.PictureBox();
+            this.htmlPanel = new Wisej.Web.HtmlPanel();
+            this.chartJS = new Wisej.Web.Ext.ChartJS.ChartJS();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.pnlDemo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnCaptureChart);
+            this.flowLayoutPanel1.Controls.Add(this.btnHtmlPanel);
+            this.flowLayoutPanel1.Controls.Add(this.btnCapturePicBox);
+            this.flowLayoutPanel1.Controls.Add(this.btnCapturePanel);
+            this.flowLayoutPanel1.Controls.Add(this.btnCaptureDemo);
+            this.flowLayoutPanel1.Controls.Add(this.btnCaptureWindow);
+            this.flowLayoutPanel1.Dock = Wisej.Web.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(146, 474);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // btnCaptureChart
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnCaptureChart, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnCaptureChart, true);
+            this.btnCaptureChart.Location = new System.Drawing.Point(3, 3);
+            this.btnCaptureChart.Name = "btnCaptureChart";
+            this.btnCaptureChart.Size = new System.Drawing.Size(140, 58);
+            this.btnCaptureChart.TabIndex = 0;
+            this.btnCaptureChart.Text = "Capture Chart";
+            this.btnCaptureChart.Click += new System.EventHandler(this.btnCaptureChart_Click);
+            // 
+            // btnHtmlPanel
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnHtmlPanel, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnHtmlPanel, true);
+            this.btnHtmlPanel.Location = new System.Drawing.Point(3, 67);
+            this.btnHtmlPanel.Name = "btnHtmlPanel";
+            this.btnHtmlPanel.Size = new System.Drawing.Size(140, 58);
+            this.btnHtmlPanel.TabIndex = 1;
+            this.btnHtmlPanel.Text = "Capture HtmlPanel ";
+            this.btnHtmlPanel.Click += new System.EventHandler(this.btnHtmlPanel_Click);
+            // 
+            // btnCapturePicBox
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnCapturePicBox, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnCapturePicBox, true);
+            this.btnCapturePicBox.Location = new System.Drawing.Point(3, 131);
+            this.btnCapturePicBox.Name = "btnCapturePicBox";
+            this.btnCapturePicBox.Size = new System.Drawing.Size(140, 58);
+            this.btnCapturePicBox.TabIndex = 2;
+            this.btnCapturePicBox.Text = "Capture PictureBox";
+            this.btnCapturePicBox.Click += new System.EventHandler(this.btnCapturePicBox_Click);
+            // 
+            // btnCapturePanel
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnCapturePanel, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnCapturePanel, true);
+            this.btnCapturePanel.Location = new System.Drawing.Point(3, 195);
+            this.btnCapturePanel.Name = "btnCapturePanel";
+            this.btnCapturePanel.Size = new System.Drawing.Size(140, 58);
+            this.btnCapturePanel.TabIndex = 3;
+            this.btnCapturePanel.Text = "Capture Panel";
+            this.btnCapturePanel.Click += new System.EventHandler(this.btnCapturePanel_Click);
+            // 
+            // btnCaptureDemo
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnCaptureDemo, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnCaptureDemo, true);
+            this.btnCaptureDemo.Location = new System.Drawing.Point(3, 259);
+            this.btnCaptureDemo.Name = "btnCaptureDemo";
+            this.btnCaptureDemo.Size = new System.Drawing.Size(140, 58);
+            this.btnCaptureDemo.TabIndex = 4;
+            this.btnCaptureDemo.Text = "Capture Demo ";
+            this.btnCaptureDemo.Click += new System.EventHandler(this.btnCaptureDemo_Click);
+            // 
+            // btnCaptureWindow
+            // 
+            this.flowLayoutPanel1.SetFillWeight(this.btnCaptureWindow, 1);
+            this.flowLayoutPanel1.SetFlowBreak(this.btnCaptureWindow, true);
+            this.btnCaptureWindow.Location = new System.Drawing.Point(3, 323);
+            this.btnCaptureWindow.Name = "btnCaptureWindow";
+            this.btnCaptureWindow.Size = new System.Drawing.Size(140, 58);
+            this.btnCaptureWindow.TabIndex = 5;
+            this.btnCaptureWindow.Text = "Capture browser window";
+            this.btnCaptureWindow.Click += new System.EventHandler(this.btnCaptureWindow_Click);
+            // 
+            // pnlDemo
+            // 
+            this.pnlDemo.Anchor = Wisej.Web.AnchorStyles.None;
+            this.pnlDemo.BackColor = System.Drawing.Color.White;
+            this.pnlDemo.Controls.Add(this.pictureBox);
+            this.pnlDemo.Controls.Add(this.htmlPanel);
+            this.pnlDemo.Controls.Add(this.chartJS);
+            this.pnlDemo.Location = new System.Drawing.Point(166, 31);
+            this.pnlDemo.Name = "pnlDemo";
+            this.pnlDemo.ShowCloseButton = false;
+            this.pnlDemo.ShowHeader = true;
+            this.pnlDemo.Size = new System.Drawing.Size(1228, 416);
+            this.pnlDemo.TabIndex = 5;
+            this.pnlDemo.Text = "Panel";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.ImageSource = "resource.wx/Wisej.DemoBrowser.Html2Canvas/wisej.png";
+            this.pictureBox.Location = new System.Drawing.Point(10, 252);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(213, 125);
+            this.pictureBox.SizeMode = Wisej.Web.PictureBoxSizeMode.Zoom;
+            // 
+            // htmlPanel
+            // 
+            this.htmlPanel.Anchor = Wisej.Web.AnchorStyles.None;
+            this.htmlPanel.Html = resources.GetString("htmlPanel.Html");
+            this.htmlPanel.Location = new System.Drawing.Point(236, 10);
+            this.htmlPanel.Name = "htmlPanel";
+            this.htmlPanel.Size = new System.Drawing.Size(989, 367);
+            // 
+            // chartJS
+            // 
+            this.chartJS.Anchor = Wisej.Web.AnchorStyles.None;
+            this.chartJS.AppearanceKey = "chartjs";
+            this.chartJS.ChartType = Wisej.Web.Ext.ChartJS.ChartType.PolarArea;
+            this.chartJS.Labels = new string[] {
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"};
+            this.chartJS.Location = new System.Drawing.Point(10, 10);
+            this.chartJS.Margin = new Wisej.Web.Padding(10);
+            this.chartJS.MinimumSize = new System.Drawing.Size(200, 200);
+            this.chartJS.Name = "chartJS";
+            this.chartJS.Options.DataLabel.BackgroundColor = System.Drawing.Color.Empty;
+            this.chartJS.Options.DataLabel.BorderColor = System.Drawing.Color.Empty;
+            this.chartJS.Options.DataLabel.Color = System.Drawing.Color.Black;
+            this.chartJS.Options.DataLabel.Opacity = 1F;
+            this.chartJS.Options.DataLabel.TextAlign = Wisej.Web.Ext.ChartJS.DataLabelTextAlignment.Start;
+            this.chartJS.Options.DataLabel.TextShadowColor = System.Drawing.Color.Empty;
+            this.chartJS.Options.DataLabel.TextStrokeColor = System.Drawing.Color.Empty;
+            this.chartJS.Options.Legend.Display = false;
+            this.chartJS.Size = new System.Drawing.Size(213, 227);
+            this.chartJS.TabIndex = 9;
+            this.chartJS.Text = "Polar Area Chart";
+            // 
+            // Features
+            // 
+            this.Controls.Add(this.pnlDemo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(1079, 285);
+            this.Name = "Features";
+            this.Load += new System.EventHandler(this.Features_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.pnlDemo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Web.FlowLayoutPanel flowLayoutPanel1;
+        private Web.Button btnCaptureChart;
+        private Web.Button btnHtmlPanel;
+        private Web.Button btnCapturePicBox;
+        private Web.Panel pnlDemo;
+        private Web.HtmlPanel htmlPanel;
+        private Web.Ext.ChartJS.ChartJS chartJS;
+        private Web.PictureBox pictureBox;
+        private Web.Button btnCapturePanel;
+        private Web.Button btnCaptureDemo;
+        private Web.Button btnCaptureWindow;
+    }
+}
