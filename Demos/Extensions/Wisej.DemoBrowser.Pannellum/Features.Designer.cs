c53bd501f71262b24ab2c8d2fb059bdbfa8c9c2b
Alaa Trigui <alaa.trigui@outlook.com>
2022-03-07 23:36:45 +0100

    Added Pannellum demo

---
+++ Demos/Extensions/Wisej.DemoBrowser.Pannellum/Features.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Wisej.DemoBrowser.Pannellum
+{
+    partial class Features
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelExamples = new Wisej.Web.FlowLayoutPanel();
+            this.btnSimple = new Wisej.Web.Button();
+            this.btnAutoRotate = new Wisej.Web.Button();
+            this.btnHeatMap = new Wisej.Web.Button();
+            this.btnCubeMap = new Wisej.Web.Button();
+            this.btnMutlires = new Wisej.Web.Button();
+            this.pannellum = new Wisej.Web.Ext.Pannellum.Pannellum();
+            this.flowLayoutPanelExamples.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelExamples
+            // 
+            this.flowLayoutPanelExamples.AppearanceKey = "panel-demo";
+            this.flowLayoutPanelExamples.AutoShow = Wisej.Web.PanelAutoShowMode.OnPointerOver;
+            this.flowLayoutPanelExamples.BorderStyle = Wisej.Web.BorderStyle.Solid;
+            this.flowLayoutPanelExamples.Collapsed = true;
+            this.flowLayoutPanelExamples.CollapseSide = Wisej.Web.HeaderPosition.Left;
+            this.flowLayoutPanelExamples.Controls.Add(this.btnSimple);
+            this.flowLayoutPanelExamples.Controls.Add(this.btnAutoRotate);
+            this.flowLayoutPanelExamples.Controls.Add(this.btnHeatMap);
+            this.flowLayoutPanelExamples.Controls.Add(this.btnCubeMap);
+            this.flowLayoutPanelExamples.Controls.Add(this.btnMutlires);
+            this.flowLayoutPanelExamples.CssStyle = "border-right: 1px solid #E5E8E9;";
+            this.flowLayoutPanelExamples.Dock = Wisej.Web.DockStyle.Left;
+            this.flowLayoutPanelExamples.HeaderPosition = Wisej.Web.HeaderPosition.Left;
+            this.flowLayoutPanelExamples.HeaderSize = 40;
+            this.flowLayoutPanelExamples.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelExamples.Name = "flowLayoutPanelExamples";
+            this.flowLayoutPanelExamples.Padding = new Wisej.Web.Padding(16, 0, 0, 0);
+            this.flowLayoutPanelExamples.RestoreBounds = new System.Drawing.Rectangle(0, 0, 232, 474);
+            this.flowLayoutPanelExamples.ShowHeader = true;
+            this.flowLayoutPanelExamples.Size = new System.Drawing.Size(40, 474);
+            this.flowLayoutPanelExamples.TabIndex = 9;
+            this.flowLayoutPanelExamples.Text = "Examples";
+            // 
+            // btnSimple
+            // 
+            this.btnSimple.AppearanceKey = "button-demo";
+            this.btnSimple.CharacterCasing = Wisej.Web.CharacterCasing.Capitalize;
+            this.flowLayoutPanelExamples.SetFillWeight(this.btnSimple, 1);
+            this.flowLayoutPanelExamples.SetFlowBreak(this.btnSimple, true);
+            this.btnSimple.Focusable = false;
+            this.btnSimple.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSimple.ImageSource = "icon-right";
+            this.btnSimple.Location = new System.Drawing.Point(19, 3);
+            this.btnSimple.Name = "btnSimple";
+            this.btnSimple.Size = new System.Drawing.Size(170, 37);
+            this.btnSimple.TabIndex = 0;
+            this.btnSimple.Text = "Simple";
+            this.btnSimple.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSimple.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnAutoRotate
+            // 
+            this.btnAutoRotate.AppearanceKey = "button-demo";
+            this.btnAutoRotate.CharacterCasing = Wisej.Web.CharacterCasing.Capitalize;
+            this.flowLayoutPanelExamples.SetFillWeight(this.btnAutoRotate, 1);
+            this.flowLayoutPanelExamples.SetFlowBreak(this.btnAutoRotate, true);
+            this.btnAutoRotate.Focusable = false;
+            this.btnAutoRotate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAutoRotate.ImageSource = "icon-right";
+            this.btnAutoRotate.Location = new System.Drawing.Point(19, 46);
+            this.btnAutoRotate.Name = "btnAutoRotate";
+            this.btnAutoRotate.Size = new System.Drawing.Size(170, 37);
+            this.btnAutoRotate.TabIndex = 1;
+            this.btnAutoRotate.Text = "Auto rotate";
+            this.btnAutoRotate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAutoRotate.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnHeatMap
+            // 
+            this.btnHeatMap.AppearanceKey = "button-demo";
+            this.btnHeatMap.CharacterCasing = Wisej.Web.CharacterCasing.Capitalize;
+            this.flowLayoutPanelExamples.SetFillWeight(this.btnHeatMap, 1);
+            this.flowLayoutPanelExamples.SetFlowBreak(this.btnHeatMap, true);
+            this.btnHeatMap.Focusable = false;
+            this.btnHeatMap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHeatMap.ImageSource = "icon-right";
+            this.btnHeatMap.Location = new System.Drawing.Point(19, 89);
+            this.btnHeatMap.Name = "btnHeatMap";
+            this.btnHeatMap.Size = new System.Drawing.Size(170, 37);
+            this.btnHeatMap.TabIndex = 2;
+            this.btnHeatMap.Text = "Hot spots";
+            this.btnHeatMap.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHeatMap.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnCubeMap
+            // 
+            this.btnCubeMap.AppearanceKey = "button-demo";
+            this.btnCubeMap.CharacterCasing = Wisej.Web.CharacterCasing.Capitalize;
+            this.flowLayoutPanelExamples.SetFillWeight(this.btnCubeMap, 1);
+            this.flowLayoutPanelExamples.SetFlowBreak(this.btnCubeMap, true);
+            this.btnCubeMap.Focusable = false;
+            this.btnCubeMap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCubeMap.ImageSource = "icon-right";
+            this.btnCubeMap.Location = new System.Drawing.Point(19, 132);
+            this.btnCubeMap.Name = "btnCubeMap";
+            this.btnCubeMap.Size = new System.Drawing.Size(170, 37);
+            this.btnCubeMap.TabIndex = 3;
+            this.btnCubeMap.Text = "Cube map";
+            this.btnCubeMap.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCubeMap.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnMutlires
+            // 
+            this.btnMutlires.AppearanceKey = "button-demo";
+            this.btnMutlires.CharacterCasing = Wisej.Web.CharacterCasing.Capitalize;
+            this.flowLayoutPanelExamples.SetFillWeight(this.btnMutlires, 1);
+            this.flowLayoutPanelExamples.SetFlowBreak(this.btnMutlires, true);
+            this.btnMutlires.Focusable = false;
+            this.btnMutlires.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMutlires.ImageSource = "icon-right";
+            this.btnMutlires.Location = new System.Drawing.Point(19, 175);
+            this.btnMutlires.Name = "btnMutlires";
+            this.btnMutlires.Size = new System.Drawing.Size(170, 37);
+            this.btnMutlires.TabIndex = 4;
+            this.btnMutlires.Text = "multires";
+            this.btnMutlires.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMutlires.Click += new System.EventHandler(this.button_Click);
+            // 
+            // pannellum
+            // 
+            this.pannellum.Dock = Wisej.Web.DockStyle.Fill;
+            this.pannellum.Location = new System.Drawing.Point(40, 0);
+            this.pannellum.Name = "pannellum";
+            this.pannellum.Size = new System.Drawing.Size(1382, 474);
+            this.pannellum.TabIndex = 10;
+            this.pannellum.Text = "pannellum1";
+            this.pannellum.WidgetEvent += new Wisej.Web.WidgetEventHandler(this.pannellum_WidgetEvent);
+            // 
+            // Features
+            // 
+            this.Controls.Add(this.pannellum);
+            this.Controls.Add(this.flowLayoutPanelExamples);
+            this.Name = "Features";
+            this.flowLayoutPanelExamples.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Web.FlowLayoutPanel flowLayoutPanelExamples;
+        private Web.Button btnSimple;
+        private Web.Button btnAutoRotate;
+        private Web.Button btnHeatMap;
+        private Web.Button btnCubeMap;
+        private Web.Button btnMutlires;
+        private Web.Ext.Pannellum.Pannellum pannellum;
+    }
+}
---
+++ Demos/Extensions/Wisej.DemoBrowser.Pannellum/Features.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using Wisej.DemoBrowser.Common;
+using Wisej.Web;
+
+namespace Wisej.DemoBrowser.Pannellum
+{
+    public partial class Features : DemoView
+    {
+        public Features()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button) sender;
+            switch (button.Text)
+            {
+                case "Auto rotate":
+                {
+                    this.pannellum.Options = new
+                    {
+                        type = "equirectangular",
+                        autoLoad = true,
+                        autoRotate = -2,
+                        panorama = "https://pannellum.org/images/alma.jpg"
+                    };
+                        break;
+                }
+                case "Hot spots":
+                {
+                    this.pannellum.Options = new
+                    {
+                        type = "equirectangular",
+                        autoLoad = true,
+                        autoRotate = -2,
+                        panorama = "https://pannellum.org/images/bma-1.jpg",
+
+                        hotSpots = new object[] {
+                            new {
+                                pitch= 14.1,
+                                yaw= 1.5,
+                                type= "info",
+                                text= "Baltimore Museum of Art",
+                                url = "https://artbma.org/"
+
+                            },
+                            new {
+                                pitch= -9.4,
+                                yaw= 222.6,
+                                type= "info",
+                                text= "Art Museum Drive"
+                            },
+                            new {
+                                pitch= -0.9,
+                                yaw= 144.4,
+                                type= "info",
+                                text= "North Charles Street"
+                            }
+                        }
+                    };
+                    break;
+                }
+                case "Cube map":
+                {
+                    this.pannellum.Options = new
+                    {
+                        type = "cubemap",
+                        autoLoad = true,
+                        cubeMap = new[] {
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face0.jpg",
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face1.jpg",
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face2.jpg",
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face3.jpg",
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face4.jpg",
+                            "https://pannellum.org/images/wyman-park-playground-cubic/face5.jpg" }
+                    };
+                        break;
+                }
+                case "multires":
+                {
+                    this.pannellum.Options = new
+                    {
+                        type = "multires",
+                        autoLoad = true,
+                        multiRes = new
+                        {
+                            basePath = "https://pannellum.org/images/multires/library",
+                            path = "/%l/%s%y_%x",
+                            fallbackPath = "/fallback/%s",
+                            extension = "jpg",
+                            tileResolution = 512,
+                            maxLevel = 6,
+                            cubeResolution = 8432
+                        }
+                    };
+                    break;
+                }
+                default:
+                {
+                    this.pannellum.Options = new
+                    {
+                        type = "equirectangular",
+                        autoLoad = true,
+                        panorama = "https://pannellum.org/images/alma.jpg"
+                    };
+                        break;
+                }
+            }
+
+            if (this._selectedButton != null)
+                UnselectButton(this._selectedButton);
+
+            this._selectedButton = button;
+
+            SelectButton(button);
+        }
+
+        private Button _selectedButton;
+        private void SelectButton(Button button)
+        {
+            button.ForeColor = Color.FromArgb(0, 56, 160);
+        }
+
+        private void UnselectButton(Button button)
+        {
+            button.ForeColor = Color.Black;
+        }
+
+        private void pannellum_WidgetEvent(object sender, WidgetEventArgs e)
+        {
+            if (e.Type == "hotspot")
+                AlertBox.Show(e.Data.text);
+        }
+    }
+}
