9eb910fbc4d1b8ec8219423ee9701fce1d1aa03e
LRufenacht <llrufenacht@gmail.com>
2022-04-05 16:08:07 -0400

    cleanup.

--- Demos/IconPacks/Wisej.DemoBrowser.BootstrapIcons/Features.Designer.cs
+++ Demos/IconPacks/Wisej.DemoBrowser.BootstrapIcons/Features.Designer.cs
@@ -29,39 +29,38 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.listViewIcons = new Wisej.Web.ListView();
-			this.imageListIcons = new Wisej.Web.ImageList(this.components);
-			this.SuspendLayout();
-			// 
-			// listViewIcons
-			// 
-			this.listViewIcons.Anchor = ((Wisej.Web.AnchorStyles)((((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom) 
+            this.components = new System.ComponentModel.Container();
+            this.listViewIcons = new Wisej.Web.ListView();
+            this.imageListIcons = new Wisej.Web.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // listViewIcons
+            // 
+            this.listViewIcons.Anchor = ((Wisej.Web.AnchorStyles)((((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom) 
             | Wisej.Web.AnchorStyles.Left) 
             | Wisej.Web.AnchorStyles.Right)));
-			this.listViewIcons.ItemSize = new System.Drawing.Size(112, 128);
-			this.listViewIcons.LabelWrap = true;
-			this.listViewIcons.LargeImageList = this.imageListIcons;
-			this.listViewIcons.Location = new System.Drawing.Point(58, 26);
-			this.listViewIcons.Name = "listViewIcons";
-			this.listViewIcons.SelectionMode = Wisej.Web.SelectionMode.None;
-			this.listViewIcons.Size = new System.Drawing.Size(1307, 423);
-			this.listViewIcons.TabIndex = 0;
-			this.listViewIcons.VirtualMode = true;
-			this.listViewIcons.SelectedIndexChanged += new System.EventHandler(this.listViewIcons_SelectedIndexChanged);
-			this.listViewIcons.RetrieveVirtualItem += new Wisej.Web.RetrieveVirtualItemEventHandler(this.listViewIcons_RetrieveVirtualItem);
-			this.listViewIcons.Appear += new System.EventHandler(this.listViewIcons_Appear);
-			// 
-			// imageListIcons
-			// 
-			this.imageListIcons.ImageSize = new System.Drawing.Size(30, 30);
-			// 
-			// Features
-			// 
-			this.Controls.Add(this.listViewIcons);
-			this.Name = "Features";
-			this.Load += new System.EventHandler(this.Features_Load);
-			this.ResumeLayout(false);
+            this.listViewIcons.LabelWrap = true;
+            this.listViewIcons.LargeImageList = this.imageListIcons;
+            this.listViewIcons.Location = new System.Drawing.Point(58, 26);
+            this.listViewIcons.Name = "listViewIcons";
+            this.listViewIcons.SelectionMode = Wisej.Web.SelectionMode.None;
+            this.listViewIcons.Size = new System.Drawing.Size(1307, 423);
+            this.listViewIcons.TabIndex = 0;
+            this.listViewIcons.VirtualMode = true;
+            this.listViewIcons.SelectedIndexChanged += new System.EventHandler(this.listViewIcons_SelectedIndexChanged);
+            this.listViewIcons.RetrieveVirtualItem += new Wisej.Web.RetrieveVirtualItemEventHandler(this.listViewIcons_RetrieveVirtualItem);
+            this.listViewIcons.Appear += new System.EventHandler(this.listViewIcons_Appear);
+            // 
+            // imageListIcons
+            // 
+            this.imageListIcons.ImageSize = new System.Drawing.Size(30, 30);
+            // 
+            // Features
+            // 
+            this.Controls.Add(this.listViewIcons);
+            this.Name = "Features";
+            this.Load += new System.EventHandler(this.Features_Load);
+            this.ResumeLayout(false);
 
 		}
 
