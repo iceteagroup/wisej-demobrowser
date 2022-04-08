6046cbeccccbbfc5187b2156b563f32717146fc9
LRufenacht <llrufenacht@gmail.com>
2022-04-08 15:27:06 -0400

    open sans prompt.

--- Wisej.DemoBrowser/Program.cs
+++ Wisej.DemoBrowser/Program.cs
@@ -1,4 +1,5 @@
-﻿using Wisej.Web;
+﻿using System.Drawing.Text;
+using Wisej.Web;
 
 namespace Wisej.DemoBrowser
 {
@@ -9,8 +10,22 @@ namespace Wisej.DemoBrowser
 		/// </summary>
 		static void Main()
 		{
+			// check font installed?
+			if (!CheckOpenSansInstalled())
+				Web.MessageBox.Show("Install <a target='_blank' href='https://fonts.google.com/specimen/Open+Sans'>Open Sans</a> for an optimal experience.<br>", allowHtml: true);
+
 			Application.MainPage = new MainView();
 			Application.SessionTimeout += Application_SessionTimeout;
+		}
+
+		private static bool CheckOpenSansInstalled()
+        {
+			var fontsCollection = new InstalledFontCollection();
+			foreach (var fontFamily in fontsCollection.Families)
+				if (fontFamily.Name == "Open Sans") 
+					return true;
+
+			return false;
 		}
 
 		private static void Application_SessionTimeout(object sender, System.ComponentModel.HandledEventArgs e)
