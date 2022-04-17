3184b70d158239d77bf715a7efdeeeec3562607d
Luca <gianluca@iceteagroup.com>
2022-04-17 19:26:07 -0400

    Fixed pdf viewer and tooltip positioning.

--- Demos/Containers/Wisej.DemoBrowser.PdfViewer/Features.cs
+++ Demos/Containers/Wisej.DemoBrowser.PdfViewer/Features.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using Wisej.DemoBrowser.Common;
 using System.Linq;
 using Wisej.Web;
+using System.IO;
 
 namespace Wisej.DemoBrowser.PdfViewer
 {
@@ -19,14 +20,13 @@ namespace Wisej.DemoBrowser.PdfViewer
             var resources = assembly.GetManifestResourceNames();
             var pdfPath = resources.First((r) => r.EndsWith("Wisej.pdf"));
 
-            var pdfStream = assembly.GetManifestResourceStream(pdfPath);
-
-            pdfViewer1.PdfStream = pdfStream;
+            pdfViewer1.PdfStream = new MemoryStream();
+            assembly.GetManifestResourceStream(pdfPath).CopyTo(pdfViewer1.PdfStream);
         }
 
         private void comboBoxViewerType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.pdfViewer1.ViewerType = (PdfViewerType)Enum.Parse(typeof(PdfViewerType), this.comboBoxViewerType.SelectedText);
+            this.pdfViewer1.ViewerType = (PdfViewerType)Enum.Parse(typeof(PdfViewerType), this.comboBoxViewerType.Text);
         }
     }
 }
