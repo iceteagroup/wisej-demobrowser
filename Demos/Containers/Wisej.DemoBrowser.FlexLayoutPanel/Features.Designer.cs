c32f0b76f806c8f1b22afc5d86acf0b9638ce681
Alaa Trigui <alaa.trigui@outlook.com>
2022-04-24 00:07:47 +0200

    FlexLayoutPanel

--- Demos/Containers/Wisej.DemoBrowser.FlexLayoutPanel/Features.Designer.cs
+++ Demos/Containers/Wisej.DemoBrowser.FlexLayoutPanel/Features.Designer.cs
@@ -32,18 +32,19 @@
             this.txtUsername = new Wisej.Web.TextBox();
             this.txtEmail = new Wisej.Web.TextBox();
             this.txtPassword = new Wisej.Web.TextBox();
-            this.btnConfirm = new Wisej.Web.Button();
             this.cmbGender = new Wisej.Web.ComboBox();
             this.chkSubscribe = new Wisej.Web.CheckBox();
+            this.btnConfirm = new Wisej.Web.Button();
             this.cmbLayoutStyle = new Wisej.Web.ComboBox();
-            this.cmbHorizontalAlignement = new Wisej.Web.ComboBox();
-            this.cmbVerticalAlignement = new Wisej.Web.ComboBox();
+            this.cmbHorizontalAlignment = new Wisej.Web.ComboBox();
+            this.cmbVerticalAlignment = new Wisej.Web.ComboBox();
             this.flexLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // flexLayoutPanel3
             // 
             this.flexLayoutPanel3.Anchor = Wisej.Web.AnchorStyles.None;
+            this.flexLayoutPanel3.AutoScroll = true;
             this.flexLayoutPanel3.BorderStyle = Wisej.Web.BorderStyle.Solid;
             this.flexLayoutPanel3.Controls.Add(this.txtUsername);
             this.flexLayoutPanel3.Controls.Add(this.txtEmail);
@@ -51,6 +52,7 @@
             this.flexLayoutPanel3.Controls.Add(this.cmbGender);
             this.flexLayoutPanel3.Controls.Add(this.chkSubscribe);
             this.flexLayoutPanel3.Controls.Add(this.btnConfirm);
+            this.flexLayoutPanel3.HeaderAlignment = Wisej.Web.HorizontalAlignment.Center;
             this.flexLayoutPanel3.HorizontalAlign = Wisej.Web.HorizontalAlignment.Center;
             this.flexLayoutPanel3.LayoutStyle = Wisej.Web.FlexLayoutStyle.Vertical;
             this.flexLayoutPanel3.Location = new System.Drawing.Point(626, 6);
@@ -58,17 +60,15 @@
             this.flexLayoutPanel3.Name = "flexLayoutPanel3";
             this.flexLayoutPanel3.Padding = new Wisej.Web.Padding(3);
             this.flexLayoutPanel3.ShowCloseButton = false;
-            this.flexLayoutPanel3.ShowHeader = true;
             this.flexLayoutPanel3.Size = new System.Drawing.Size(566, 462);
             this.flexLayoutPanel3.TabIndex = 4;
             this.flexLayoutPanel3.TabStop = true;
-            this.flexLayoutPanel3.Text = "LayoutStyle Vertical";
             this.flexLayoutPanel3.VerticalAlign = Wisej.Web.VerticalAlignment.Middle;
             // 
             // txtUsername
             // 
             this.txtUsername.LabelText = "Username";
-            this.txtUsername.Location = new System.Drawing.Point(6, 41);
+            this.txtUsername.Location = new System.Drawing.Point(6, 55);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(552, 53);
             this.txtUsername.TabIndex = 2;
@@ -76,7 +76,7 @@
             // txtEmail
             // 
             this.txtEmail.LabelText = "E-Mail";
-            this.txtEmail.Location = new System.Drawing.Point(6, 110);
+            this.txtEmail.Location = new System.Drawing.Point(6, 124);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(552, 53);
             this.txtEmail.TabIndex = 3;
@@ -84,20 +84,10 @@
             // txtPassword
             // 
             this.txtPassword.LabelText = "Password";
-            this.txtPassword.Location = new System.Drawing.Point(6, 179);
+            this.txtPassword.Location = new System.Drawing.Point(6, 193);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(552, 53);
             this.txtPassword.TabIndex = 4;
-            // 
-            // btnConfirm
-            // 
-            this.btnConfirm.BackColor = System.Drawing.Color.FromArgb(105, 205, 116);
-            this.btnConfirm.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
-            this.btnConfirm.Location = new System.Drawing.Point(6, 356);
-            this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(552, 34);
-            this.btnConfirm.TabIndex = 5;
-            this.btnConfirm.Text = "Confirm";
             // 
             // cmbGender
             // 
@@ -106,21 +96,32 @@
             "Female",
             "Other"});
             this.cmbGender.LabelText = "Gender";
-            this.cmbGender.Location = new System.Drawing.Point(6, 248);
+            this.cmbGender.Location = new System.Drawing.Point(6, 262);
             this.cmbGender.Name = "cmbGender";
             this.cmbGender.Size = new System.Drawing.Size(552, 53);
             this.cmbGender.TabIndex = 6;
             // 
             // chkSubscribe
             // 
-            this.chkSubscribe.Location = new System.Drawing.Point(6, 317);
+            this.chkSubscribe.Location = new System.Drawing.Point(6, 331);
             this.chkSubscribe.Name = "chkSubscribe";
             this.chkSubscribe.Size = new System.Drawing.Size(552, 23);
             this.chkSubscribe.TabIndex = 7;
             this.chkSubscribe.Text = "Subscribe To Newsletter";
             // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.Color.FromArgb(105, 205, 116);
+            this.btnConfirm.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+            this.btnConfirm.Location = new System.Drawing.Point(6, 370);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(552, 34);
+            this.btnConfirm.TabIndex = 5;
+            this.btnConfirm.Text = "Confirm";
+            // 
             // cmbLayoutStyle
             // 
+            this.cmbLayoutStyle.Anchor = Wisej.Web.AnchorStyles.None;
             this.cmbLayoutStyle.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList;
             this.cmbLayoutStyle.Items.AddRange(new object[] {
             "Default",
@@ -131,41 +132,47 @@
             this.cmbLayoutStyle.Name = "cmbLayoutStyle";
             this.cmbLayoutStyle.Size = new System.Drawing.Size(230, 53);
             this.cmbLayoutStyle.TabIndex = 5;
+            this.cmbLayoutStyle.SelectedIndexChanged += new System.EventHandler(this.cmbLayoutStyle_SelectedIndexChanged);
             // 
-            // cmbHorizontalAlignement
+            // cmbHorizontalAlignment
             // 
-            this.cmbHorizontalAlignement.Items.AddRange(new object[] {
+            this.cmbHorizontalAlignment.Anchor = Wisej.Web.AnchorStyles.None;
+            this.cmbHorizontalAlignment.Items.AddRange(new object[] {
             "Left",
             "Right",
             "Center"});
-            this.cmbHorizontalAlignement.LabelText = "HorizontalAlignement";
-            this.cmbHorizontalAlignement.Location = new System.Drawing.Point(231, 218);
-            this.cmbHorizontalAlignement.Name = "cmbHorizontalAlignement";
-            this.cmbHorizontalAlignement.Size = new System.Drawing.Size(230, 53);
-            this.cmbHorizontalAlignement.TabIndex = 6;
+            this.cmbHorizontalAlignment.LabelText = "HorizontalAlignment";
+            this.cmbHorizontalAlignment.Location = new System.Drawing.Point(231, 218);
+            this.cmbHorizontalAlignment.Name = "cmbHorizontalAlignment";
+            this.cmbHorizontalAlignment.Size = new System.Drawing.Size(230, 53);
+            this.cmbHorizontalAlignment.TabIndex = 6;
+            this.cmbHorizontalAlignment.SelectedIndexChanged += new System.EventHandler(this.cmbHorizontalAlignment_SelectedIndexChanged);
             // 
-            // cmbVerticalAlignement
+            // cmbVerticalAlignment
             // 
-            this.cmbVerticalAlignement.Items.AddRange(new object[] {
+            this.cmbVerticalAlignment.Anchor = Wisej.Web.AnchorStyles.None;
+            this.cmbVerticalAlignment.Items.AddRange(new object[] {
             "Top",
-            "Center",
+            "Middle",
             "Bottom"});
-            this.cmbVerticalAlignement.LabelText = "VerticalAlignement";
-            this.cmbVerticalAlignement.Location = new System.Drawing.Point(231, 298);
-            this.cmbVerticalAlignement.Name = "cmbVerticalAlignement";
-            this.cmbVerticalAlignement.Size = new System.Drawing.Size(230, 53);
-            this.cmbVerticalAlignement.TabIndex = 7;
+            this.cmbVerticalAlignment.LabelText = "VerticalAlignment";
+            this.cmbVerticalAlignment.Location = new System.Drawing.Point(231, 298);
+            this.cmbVerticalAlignment.Name = "cmbVerticalAlignment";
+            this.cmbVerticalAlignment.Size = new System.Drawing.Size(230, 53);
+            this.cmbVerticalAlignment.TabIndex = 7;
+            this.cmbVerticalAlignment.SelectedIndexChanged += new System.EventHandler(this.cmbVerticalAlignment_SelectedIndexChanged);
             // 
             // Features
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
-            this.Controls.Add(this.cmbVerticalAlignement);
-            this.Controls.Add(this.cmbHorizontalAlignement);
+            this.Controls.Add(this.cmbVerticalAlignment);
+            this.Controls.Add(this.cmbHorizontalAlignment);
             this.Controls.Add(this.cmbLayoutStyle);
             this.Controls.Add(this.flexLayoutPanel3);
             this.MinimumSize = new System.Drawing.Size(940, 400);
             this.Name = "Features";
+            this.Load += new System.EventHandler(this.Features_Load);
             this.flexLayoutPanel3.ResumeLayout(false);
             this.flexLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
@@ -182,7 +189,7 @@
         private Web.ComboBox cmbGender;
         private Web.CheckBox chkSubscribe;
         private Web.ComboBox cmbLayoutStyle;
-        private Web.ComboBox cmbHorizontalAlignement;
-        private Web.ComboBox cmbVerticalAlignement;
+        private Web.ComboBox cmbHorizontalAlignment;
+        private Web.ComboBox cmbVerticalAlignment;
     }
 }
--- Demos/Containers/Wisej.DemoBrowser.FlexLayoutPanel/Features.cs
+++ Demos/Containers/Wisej.DemoBrowser.FlexLayoutPanel/Features.cs
@@ -1,5 +1,6 @@
 ﻿using Wisej.DemoBrowser.Common;
-using System.Windows.Forms;
+using Wisej.Web;
+using System;
 
 namespace Wisej.DemoBrowser.FlexLayoutPanel
 {
@@ -8,6 +9,32 @@ namespace Wisej.DemoBrowser.FlexLayoutPanel
         public Features()
         {
             InitializeComponent();
+        }
+
+        private void Features_Load(object sender, System.EventArgs e)
+        {
+            cmbGender.SelectedIndex = 0;
+            cmbHorizontalAlignment.SelectedIndex = 0;
+            cmbLayoutStyle.SelectedIndex = 0;
+            cmbVerticalAlignment.SelectedIndex = 0;
+        }
+
+        private void cmbLayoutStyle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var layoutStyle = cmbLayoutStyle.SelectedItem.ToString();
+            this.flexLayoutPanel3.LayoutStyle = (FlexLayoutStyle)Enum.Parse(typeof(FlexLayoutStyle), layoutStyle);
+        }
+
+        private void cmbHorizontalAlignment_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var horizontalAlignment = cmbHorizontalAlignment.SelectedItem.ToString();
+            this.flexLayoutPanel3.HorizontalAlign = (HorizontalAlignment)Enum.Parse(typeof(HorizontalAlignment), horizontalAlignment);
+        }
+
+        private void cmbVerticalAlignment_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var verticalAlignment = cmbVerticalAlignment.SelectedItem.ToString();
+            this.flexLayoutPanel3.VerticalAlign = (VerticalAlignment)Enum.Parse(typeof(VerticalAlignment), verticalAlignment);
         }
     }
 }
