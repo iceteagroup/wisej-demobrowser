3126b50c0269cfe3405c3cc35c69e2fb3b209976
Alaa Trigui <alaa.trigui@outlook.com>
2022-05-24 15:50:39 +0200

    Fixed tree demo (IgniteUI).

--- Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Ignite.Demo/Component/igTree.Designer.cs
+++ Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Ignite.Demo/Component/igTree.Designer.cs
@@ -28,134 +28,133 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            Wisej.Web.Ext.Ignite.igBase.WidgetFunction widgetFunction1 = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(igTree));
-            Wisej.Web.Ext.Ignite.igBase.WidgetFunction widgetFunction2 = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction();
-            this.igTree2 = new Wisej.Web.Ext.Ignite.igTree();
-            this.igTree1 = new Wisej.Web.Ext.Ignite.igTree();
-            this.buttonClearSelection = new Wisej.Web.Button();
-            this.checkBox1 = new Wisej.Web.CheckBox();
-            this.comboBox1 = new Wisej.Web.ComboBox();
-            this.groupBox1.SuspendLayout();
-            this.panel.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.TabIndex = 3;
-            // 
-            // buttonUpdate
-            // 
-            this.buttonUpdate.TabIndex = 3;
-            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
-            // 
-            // panel
-            // 
-            this.panel.Controls.Add(this.igTree2);
-            this.panel.Controls.Add(this.igTree1);
-            this.panel.TabIndex = 7;
-            // 
-            // linkDemo
-            // 
-            this.linkDemo.TabIndex = 6;
-            this.linkDemo.Text = "https://www.igniteui.com/tree/overview";
-            // 
-            // linkApi
-            // 
-            this.linkApi.TabIndex = 5;
-            this.linkApi.Text = "http://www.igniteui.com/help/api/2019.2/ui.igtree";
-            // 
-            // linkDocs
-            // 
-            this.linkDocs.TabIndex = 4;
-            this.linkDocs.Text = "http://www.igniteui.com/help/igtree-igtree";
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
-            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
-            this.flowLayoutPanel1.Controls.Add(this.buttonClearSelection);
-            // 
-            // igTree2
-            // 
-            this.igTree2.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom)));
-            this.igTree2.Location = new System.Drawing.Point(446, 28);
-            this.igTree2.Name = "igTree2";
-            this.igTree2.Size = new System.Drawing.Size(252, 521);
-            this.igTree2.TabIndex = 1;
-            this.igTree2.Text = "igTree2";
-            widgetFunction1.Name = "customDropValidation";
-            widgetFunction1.Source = resources.GetString("widgetFunction1.Source");
-            this.igTree2.WidgetFunctions = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction[] {
+			Wisej.Web.Ext.Ignite.igBase.WidgetFunction widgetFunction1 = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(igTree));
+			Wisej.Web.Ext.Ignite.igBase.WidgetFunction widgetFunction2 = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction();
+			this.igTree2 = new Wisej.Web.Ext.Ignite.igTree();
+			this.igTree1 = new Wisej.Web.Ext.Ignite.igTree();
+			this.buttonClearSelection = new Wisej.Web.Button();
+			this.checkBox1 = new Wisej.Web.CheckBox();
+			this.comboBox1 = new Wisej.Web.ComboBox();
+			this.groupBox1.SuspendLayout();
+			this.panel.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.TabIndex = 3;
+			// 
+			// buttonUpdate
+			// 
+			this.buttonUpdate.TabIndex = 3;
+			this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+			// 
+			// panel
+			// 
+			this.panel.Controls.Add(this.igTree2);
+			this.panel.Controls.Add(this.igTree1);
+			this.panel.TabIndex = 7;
+			// 
+			// linkDemo
+			// 
+			this.linkDemo.TabIndex = 6;
+			this.linkDemo.Text = "https://www.igniteui.com/tree/overview";
+			// 
+			// linkApi
+			// 
+			this.linkApi.TabIndex = 5;
+			this.linkApi.Text = "http://www.igniteui.com/help/api/2019.2/ui.igtree";
+			// 
+			// linkDocs
+			// 
+			this.linkDocs.TabIndex = 4;
+			this.linkDocs.Text = "http://www.igniteui.com/help/igtree-igtree";
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.checkBox1);
+			this.flowLayoutPanel1.Controls.Add(this.comboBox1);
+			this.flowLayoutPanel1.Controls.Add(this.buttonClearSelection);
+			// 
+			// igTree2
+			// 
+			this.igTree2.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom)));
+			this.igTree2.Location = new System.Drawing.Point(446, 28);
+			this.igTree2.Name = "igTree2";
+			this.igTree2.Size = new System.Drawing.Size(252, 521);
+			this.igTree2.TabIndex = 1;
+			this.igTree2.Text = "igTree2";
+			widgetFunction1.Name = "customDropValidation";
+			widgetFunction1.Source = resources.GetString("widgetFunction1.Source");
+			this.igTree2.WidgetFunctions = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction[] {
         widgetFunction1};
-            // 
-            // igTree1
-            // 
-            this.igTree1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom)));
-            this.igTree1.Location = new System.Drawing.Point(190, 28);
-            this.igTree1.Name = "igTree1";
-            this.igTree1.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse(resources.GetString("igTree1.Options"))));
-            this.igTree1.Size = new System.Drawing.Size(250, 521);
-            this.igTree1.TabIndex = 0;
-            this.igTree1.Text = "igTree1";
-            widgetFunction2.Name = "customDropValidation";
-            widgetFunction2.Source = resources.GetString("widgetFunction2.Source");
-            this.igTree1.WidgetFunctions = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction[] {
+			// 
+			// igTree1
+			// 
+			this.igTree1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom)));
+			this.igTree1.Location = new System.Drawing.Point(190, 28);
+			this.igTree1.Name = "igTree1";
+			this.igTree1.Size = new System.Drawing.Size(250, 521);
+			this.igTree1.TabIndex = 0;
+			this.igTree1.Text = "igTree1";
+			widgetFunction2.Name = "customDropValidation";
+			widgetFunction2.Source = resources.GetString("widgetFunction2.Source");
+			this.igTree1.WidgetFunctions = new Wisej.Web.Ext.Ignite.igBase.WidgetFunction[] {
         widgetFunction2};
-            // 
-            // buttonClearSelection
-            // 
-            this.flowLayoutPanel1.SetFillWeight(this.buttonClearSelection, 1);
-            this.flowLayoutPanel1.SetFlowBreak(this.buttonClearSelection, true);
-            this.buttonClearSelection.Location = new System.Drawing.Point(3, 148);
-            this.buttonClearSelection.Margin = new Wisej.Web.Padding(3, 16, 3, 3);
-            this.buttonClearSelection.Name = "buttonClearSelection";
-            this.buttonClearSelection.Size = new System.Drawing.Size(212, 40);
-            this.buttonClearSelection.TabIndex = 2;
-            this.buttonClearSelection.Text = "Clear Selection";
-            this.buttonClearSelection.Click += new System.EventHandler(this.buttonClearSelection_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.Appearance = Wisej.Web.Appearance.Switch;
-            this.checkBox1.AutoSize = false;
-            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox1.Checked = true;
-            this.flowLayoutPanel1.SetFillWeight(this.checkBox1, 1);
-            this.flowLayoutPanel1.SetFlowBreak(this.checkBox1, true);
-            this.checkBox1.Location = new System.Drawing.Point(3, 3);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(212, 50);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Single Branch Expand";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList;
-            this.flowLayoutPanel1.SetFillWeight(this.comboBox1, 1);
-            this.flowLayoutPanel1.SetFlowBreak(this.comboBox1, true);
-            this.comboBox1.Items.AddRange(new object[] {
+			// 
+			// buttonClearSelection
+			// 
+			this.flowLayoutPanel1.SetFillWeight(this.buttonClearSelection, 1);
+			this.flowLayoutPanel1.SetFlowBreak(this.buttonClearSelection, true);
+			this.buttonClearSelection.Location = new System.Drawing.Point(3, 148);
+			this.buttonClearSelection.Margin = new Wisej.Web.Padding(3, 16, 3, 3);
+			this.buttonClearSelection.Name = "buttonClearSelection";
+			this.buttonClearSelection.Size = new System.Drawing.Size(212, 40);
+			this.buttonClearSelection.TabIndex = 2;
+			this.buttonClearSelection.Text = "Clear Selection";
+			this.buttonClearSelection.Click += new System.EventHandler(this.buttonClearSelection_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Appearance = Wisej.Web.Appearance.Switch;
+			this.checkBox1.AutoSize = false;
+			this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.checkBox1.Checked = true;
+			this.flowLayoutPanel1.SetFillWeight(this.checkBox1, 1);
+			this.flowLayoutPanel1.SetFlowBreak(this.checkBox1, true);
+			this.checkBox1.Location = new System.Drawing.Point(3, 3);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(212, 50);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "Single Branch Expand";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList;
+			this.flowLayoutPanel1.SetFillWeight(this.comboBox1, 1);
+			this.flowLayoutPanel1.SetFlowBreak(this.comboBox1, true);
+			this.comboBox1.Items.AddRange(new object[] {
             "off",
             "biState",
             "triState"});
-            this.comboBox1.LabelText = "Checkbox Mode";
-            this.comboBox1.Location = new System.Drawing.Point(3, 72);
-            this.comboBox1.Margin = new Wisej.Web.Padding(3, 16, 3, 3);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(212, 57);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.Text = "triState";
-            // 
-            // igTree
-            // 
-            this.Name = "igTree";
-            this.Load += new System.EventHandler(this.igTree_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.panel.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
-            this.ResumeLayout(false);
+			this.comboBox1.LabelText = "Checkbox Mode";
+			this.comboBox1.Location = new System.Drawing.Point(3, 72);
+			this.comboBox1.Margin = new Wisej.Web.Padding(3, 16, 3, 3);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(212, 57);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.Text = "triState";
+			// 
+			// igTree
+			// 
+			this.Name = "igTree";
+			this.Load += new System.EventHandler(this.igTree_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.panel.ResumeLayout(false);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
 
 		}
 
--- Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Ignite.Demo/Component/igTree.cs
+++ Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Ignite.Demo/Component/igTree.cs
@@ -16,231 +16,10 @@ namespace Wisej.Web.Ext.Ignite.Demo.Component
 		private void igTree_Load(object sender, EventArgs e)
 		{
 			//igTree1 configuration
-			//igTree1.Options.dragAndDrop = true;
-			//igTree1.Options.checkboxMode = "triState";
-			//igTree1.Options.singleBranchExpand = true;
-			//igTree1.Options.dataSource = new dynamic[]
-			//{
-			//	new
-			//	{
-			//		Text = "Computer", Value = "Folder", ImageUrl = "Images/Tree/computer.png", Folder = new dynamic[]
-			//		{
-			//			new
-			//			{
-			//				Text = "Music", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder = new dynamic[]
-			//				{
-			//					new
-			//					{
-			//						Text = "Y.Malmsteen", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
-			//							new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "Making Love", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Rising Force", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Fire and Ice", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								}
-			//							}
-			//					},
-			//					new
-			//					{
-			//						Text = "WhiteSnake", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
-			//							new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "Trouble", Value = "File", ImageUrl = "Images/Tree/music.png",
-			//									Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Bad Boys", Value = "File", ImageUrl = "Images/Tree/music.png",
-			//									Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Is This Love", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								}
-			//							}
-			//					},
-			//					new
-			//					{
-			//						Text = "AC/DC", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
-			//							new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "ThunderStruck", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "T.N.T.", Value = "File", ImageUrl = "Images/Tree/music.png",
-			//									Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "The Jack", Value = "File", ImageUrl = "Images/Tree/music.png",
-			//									Folder = ""
-			//								}
-			//							}
-			//					},
-			//					new
-			//					{
-			//						Text = "Rock", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
-			//							new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "Bon Jovi - Always", Value = "File",
-			//									ImageUrl = "Images/Tree/music.png", Folder = ""
-			//								}
-			//							}
-			//					}
-			//				}
-			//			},
-			//			new
-			//			{
-			//				Text = "My Documents", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
-			//				Folder = new dynamic[]
-			//				{
-			//					new
-			//					{
-			//						Text = "2009", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
-			//						Folder = new dynamic[]
-			//						{
-			//							new
-			//							{
-			//								Text = "Month Report", Value = "File",
-			//								ImageUrl = "Images/Tree/documents.png", Folder = ""
-			//							},
-			//							new
-			//							{
-			//								Text = "Year Report", Value = "File",
-			//								ImageUrl = "Images/Tree/documents.png", Folder = ""
-			//							}
-			//						}
-			//					},
-			//					new
-			//					{
-			//						Text = "2010", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
-			//						Folder = new dynamic[]
-			//						{
-			//							new
-			//							{
-			//								Text = "Month Report", Value = "File",
-			//								ImageUrl = "Images/Tree/documents.png", Folder = ""
-			//							},
-			//							new
-			//							{
-			//								Text = "Year Report", Value = "File",
-			//								ImageUrl = "Images/Tree/documents.png", Folder = ""
-			//							}
-			//						}
-			//					}
-			//				}
-			//			},
-			//			new
-			//			{
-			//				Text = "Pictures", Value = "Folder", ImageUrl = "Images/Tree/coins.png", Folder =
-			//					new dynamic[]
-			//					{
-			//						new
-			//						{
-			//							Text = "BirthDay2009", Value = "Folder", ImageUrl = "Images/Tree/coins.png",
-			//							Folder = new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "Picture1", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Picture2", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Picture3", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Picture4", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								}
-			//							}
-			//						},
-			//						new
-			//						{
-			//							Text = "BirthDay2010", Value = "Folder", ImageUrl = "Images/Tree/coins.png",
-			//							Folder = new dynamic[]
-			//							{
-			//								new
-			//								{
-			//									Text = "Picture1", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Picture2", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								},
-			//								new
-			//								{
-			//									Text = "Picture3", Value = "File",
-			//									ImageUrl = "Images/Tree/coins_add.png", Folder = ""
-			//								}
-			//							}
-			//						}
-			//					}
-			//			},
-			//			new
-			//			{
-			//				Text = "Network", Value = "Folder", ImageUrl = "Images/Tree/door.png", Folder =
-			//					new dynamic[]
-			//					{
-			//						new
-			//						{
-			//							Text = "Archive", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
-			//							Folder = ""
-			//						},
-			//						new
-			//						{
-			//							Text = "BackUp", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
-			//							Folder = ""
-			//						},
-			//						new
-			//						{
-			//							Text = "FTP", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
-			//							Folder = ""
-			//						}
-			//					}
-			//			},
-			//			new {Text = "Deleted", Value = "Folder", ImageUrl = "Images/Tree/bin_empty.png", Folder = ""}
-			//		}
-			//	}
-			//};
-			//igTree1.Options.dataSourceType = "json";
-			//igTree1.Options.initialExpandDepth = 0;
-			//igTree1.Options.pathSeparator = ".";
-
-			//igTree2 Configuration
-			igTree2.Options.dragAndDrop = true;
-			igTree2.Options.checkboxMode = "triState";
-			igTree2.Options.singleBranchExpand = true;
-			igTree2.Options.dataSource = new dynamic[]
+			this.igTree1.Options.dragAndDrop = true;
+			this.igTree1.Options.checkboxMode = "triState";
+			this.igTree1.Options.singleBranchExpand = true;
+			this.igTree1.Options.dataSource = new dynamic[]
 			{
 				new
 				{
@@ -453,9 +232,263 @@ namespace Wisej.Web.Ext.Ignite.Demo.Component
 					}
 				}
 			};
-			igTree2.Options.dataSourceType = "json";
-			igTree2.Options.initialExpandDepth = 0;
-			igTree2.Options.pathSeparator = ".";
+			this.igTree1.Options.dataSourceType = "json";
+			this.igTree1.Options.initialExpandDepth = 0;
+			this.igTree1.Options.pathSeparator = ".";
+
+			this.igTree1.Options.bindings = new
+			{
+				textKey = "Text",
+				valueKey = "Value",
+				imageUrlKey = "ImageUrl",
+				childDataProperty = "Folder",
+			};
+
+			this.igTree1.Options.dragAndDropSettings =
+				new
+				{
+					allowDrop = true,
+					customDropValidation = "()=>customDropValidation"
+
+				};
+
+
+			//igTree2 Configuration
+			this.igTree2.Options.dragAndDrop = true;
+			this.igTree2.Options.checkboxMode = "triState";
+			this.igTree2.Options.singleBranchExpand = true;
+			this.igTree2.Options.dataSource = new dynamic[]
+			{
+				new
+				{
+					Text = "Computer", Value = "Folder", ImageUrl = "Images/Tree/computer.png", Folder = new dynamic[]
+					{
+						new
+						{
+							Text = "Music", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder = new dynamic[]
+							{
+								new
+								{
+									Text = "Y.Malmsteen", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
+										new dynamic[]
+										{
+											new
+											{
+												Text = "Making Love", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											},
+											new
+											{
+												Text = "Rising Force", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											},
+											new
+											{
+												Text = "Fire and Ice", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											}
+										}
+								},
+								new
+								{
+									Text = "WhiteSnake", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
+										new dynamic[]
+										{
+											new
+											{
+												Text = "Trouble", Value = "File", ImageUrl = "Images/Tree/music.png",
+												Folder = ""
+											},
+											new
+											{
+												Text = "Bad Boys", Value = "File", ImageUrl = "Images/Tree/music.png",
+												Folder = ""
+											},
+											new
+											{
+												Text = "Is This Love", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											}
+										}
+								},
+								new
+								{
+									Text = "AC/DC", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
+										new dynamic[]
+										{
+											new
+											{
+												Text = "ThunderStruck", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											},
+											new
+											{
+												Text = "T.N.T.", Value = "File", ImageUrl = "Images/Tree/music.png",
+												Folder = ""
+											},
+											new
+											{
+												Text = "The Jack", Value = "File", ImageUrl = "Images/Tree/music.png",
+												Folder = ""
+											}
+										}
+								},
+								new
+								{
+									Text = "Rock", Value = "Folder", ImageUrl = "Images/Tree/book.png", Folder =
+										new dynamic[]
+										{
+											new
+											{
+												Text = "Bon Jovi - Always", Value = "File",
+												ImageUrl = "Images/Tree/music.png", Folder = ""
+											}
+										}
+								}
+							}
+						},
+						new
+						{
+							Text = "My Documents", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
+							Folder = new dynamic[]
+							{
+								new
+								{
+									Text = "2009", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
+									Folder = new dynamic[]
+									{
+										new
+										{
+											Text = "Month Report", Value = "File",
+											ImageUrl = "Images/Tree/documents.png", Folder = ""
+										},
+										new
+										{
+											Text = "Year Report", Value = "File",
+											ImageUrl = "Images/Tree/documents.png", Folder = ""
+										}
+									}
+								},
+								new
+								{
+									Text = "2010", Value = "Folder", ImageUrl = "Images/Tree/documents-folder.png",
+									Folder = new dynamic[]
+									{
+										new
+										{
+											Text = "Month Report", Value = "File",
+											ImageUrl = "Images/Tree/documents.png", Folder = ""
+										},
+										new
+										{
+											Text = "Year Report", Value = "File",
+											ImageUrl = "Images/Tree/documents.png", Folder = ""
+										}
+									}
+								}
+							}
+						},
+						new
+						{
+							Text = "Pictures", Value = "Folder", ImageUrl = "Images/Tree/coins.png", Folder =
+								new dynamic[]
+								{
+									new
+									{
+										Text = "BirthDay2009", Value = "Folder", ImageUrl = "Images/Tree/coins.png",
+										Folder = new dynamic[]
+										{
+											new
+											{
+												Text = "Picture1", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											},
+											new
+											{
+												Text = "Picture2", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											},
+											new
+											{
+												Text = "Picture3", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											},
+											new
+											{
+												Text = "Picture4", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											}
+										}
+									},
+									new
+									{
+										Text = "BirthDay2010", Value = "Folder", ImageUrl = "Images/Tree/coins.png",
+										Folder = new dynamic[]
+										{
+											new
+											{
+												Text = "Picture1", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											},
+											new
+											{
+												Text = "Picture2", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											},
+											new
+											{
+												Text = "Picture3", Value = "File",
+												ImageUrl = "Images/Tree/coins_add.png", Folder = ""
+											}
+										}
+									}
+								}
+						},
+						new
+						{
+							Text = "Network", Value = "Folder", ImageUrl = "Images/Tree/door.png", Folder =
+								new dynamic[]
+								{
+									new
+									{
+										Text = "Archive", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
+										Folder = ""
+									},
+									new
+									{
+										Text = "BackUp", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
+										Folder = ""
+									},
+									new
+									{
+										Text = "FTP", Value = "Folder", ImageUrl = "Images/Tree/door_in.png",
+										Folder = ""
+									}
+								}
+						},
+						new {Text = "Deleted", Value = "Folder", ImageUrl = "Images/Tree/bin_empty.png", Folder = ""}
+					}
+				}
+			};
+			this.igTree2.Options.dataSourceType = "json";
+			this.igTree2.Options.initialExpandDepth = 0;
+			this.igTree2.Options.pathSeparator = ".";
+
+			this.igTree2.Options.bindings = new
+			{
+				textKey = "Text",
+				valueKey = "Value",
+				imageUrlKey = "ImageUrl",
+				childDataProperty = "Folder",
+			};
+
+			this.igTree2.Options.dragAndDropSettings =
+			new
+			{
+				allowDrop = true,
+				customDropValidation = "()=>customDropValidation"
+
+			};
 		}
 
 		private void igTree_WidgetEvent(object sender, WidgetEventArgs e)
@@ -473,11 +506,17 @@ namespace Wisej.Web.Ext.Ignite.Demo.Component
 			igTree1.Options.singleBranchExpand = checkBox1.Checked;
 
 			igTree1.Update();
+
+			igTree2.Options.checkboxMode = comboBox1.SelectedItem;
+			igTree2.Options.singleBranchExpand = checkBox1.Checked;
+
+			igTree2.Update();
 		}
 
 		private void buttonClearSelection_Click(object sender, EventArgs e)
 		{
 			igTree1.Instance.clearSelection();
+			igTree2.Instance.clearSelection();
 		}
 	}
 }
