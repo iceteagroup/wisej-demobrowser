c8404c8e59a61bc94e5ad3d068dbc4f80a862ef4
Alaa Trigui <alaa.trigui@outlook.com>
2022-06-02 23:28:05 +0200

    Added Accordion and TagTextBox demos.

---
+++ Demos/Containers/Wisej.DemoBrowser.Accordion/Features.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Wisej.DemoBrowser.Accordion
+{
+	partial class Features
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			Wisej.Web.ImageListEntry imageListEntry1 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.apple.png", "apple");
+			Wisej.Web.ImageListEntry imageListEntry2 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.banana.png", "banana");
+			Wisej.Web.ImageListEntry imageListEntry3 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.carrot.png", "carrot");
+			Wisej.Web.ImageListEntry imageListEntry4 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.cucumber.png", "cucumber");
+			Wisej.Web.ImageListEntry imageListEntry5 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.milk.png", "milk");
+			Wisej.Web.ImageListEntry imageListEntry6 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.orange.png", "orange");
+			Wisej.Web.ImageListEntry imageListEntry7 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.yogurt.png", "yogurt");
+			Wisej.Web.ImageListEntry imageListEntry8 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.dairy.png", "dairy");
+			Wisej.Web.ImageListEntry imageListEntry9 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.fruit.png", "fruit");
+			Wisej.Web.ImageListEntry imageListEntry10 = new Wisej.Web.ImageListEntry("resource.wx/Wisej.DemoBrowser.TreeView.Images.vegetables.png", "vegetables");
+			Wisej.Web.TreeNode treeNode1 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode2 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode3 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode4 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode5 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode6 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode7 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode8 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode9 = new Wisej.Web.TreeNode();
+			Wisej.Web.TreeNode treeNode10 = new Wisej.Web.TreeNode();
+			this.accordion1 = new Wisej.Web.Accordion();
+			this.accordionPanel1 = new Wisej.Web.AccordionPanel();
+			this.accordionPanel2 = new Wisej.Web.AccordionPanel();
+			this.radioButton5 = new Wisej.Web.RadioButton();
+			this.radioButton6 = new Wisej.Web.RadioButton();
+			this.radioButton7 = new Wisej.Web.RadioButton();
+			this.radioButton8 = new Wisej.Web.RadioButton();
+			this.button2 = new Wisej.Web.Button();
+			this.numericUpDown1 = new Wisej.Web.NumericUpDown();
+			this.textBox1 = new Wisej.Web.TextBox();
+			this.imageList1 = new Wisej.Web.ImageList(this.components);
+			this.treeView2 = new Wisej.Web.TreeView();
+			this.chkCollapsible = new Wisej.Web.CheckBox();
+			this.chkSelectedOnTop = new Wisej.Web.CheckBox();
+			((System.ComponentModel.ISupportInitialize)(this.accordion1)).BeginInit();
+			this.accordion1.SuspendLayout();
+			this.accordionPanel1.SuspendLayout();
+			this.accordionPanel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// accordion1
+			// 
+			this.accordion1.Anchor = Wisej.Web.AnchorStyles.None;
+			this.accordion1.Collapsible = true;
+			this.accordion1.Controls.Add(this.accordionPanel1);
+			this.accordion1.Controls.Add(this.accordionPanel2);
+			this.accordion1.Location = new System.Drawing.Point(498, 47);
+			this.accordion1.Name = "accordion1";
+			this.accordion1.SelectedIndex = 0;
+			this.accordion1.Size = new System.Drawing.Size(641, 381);
+			this.accordion1.TabIndex = 0;
+			this.accordion1.Selected += new Wisej.Web.AccordionEventHandler(this.accordion1_Selected);
+			this.accordion1.Deselected += new Wisej.Web.AccordionEventHandler(this.accordion1_Deselected);
+			// 
+			// accordionPanel1
+			// 
+			this.accordionPanel1.Controls.Add(this.radioButton5);
+			this.accordionPanel1.Controls.Add(this.radioButton6);
+			this.accordionPanel1.Controls.Add(this.radioButton7);
+			this.accordionPanel1.Controls.Add(this.radioButton8);
+			this.accordionPanel1.Controls.Add(this.button2);
+			this.accordionPanel1.Controls.Add(this.numericUpDown1);
+			this.accordionPanel1.Controls.Add(this.textBox1);
+			this.accordionPanel1.Location = new System.Drawing.Point(0, 0);
+			this.accordionPanel1.Name = "accordionPanel1";
+			this.accordionPanel1.Size = new System.Drawing.Size(639, 351);
+			this.accordionPanel1.Text = "Sales Data";
+			// 
+			// accordionPanel2
+			// 
+			this.accordionPanel2.Controls.Add(this.treeView2);
+			this.accordionPanel2.Location = new System.Drawing.Point(0, 351);
+			this.accordionPanel2.Name = "accordionPanel2";
+			this.accordionPanel2.Padding = new Wisej.Web.Padding(8);
+			this.accordionPanel2.RestoreBounds = new System.Drawing.Rectangle(0, 351, 639, 351);
+			this.accordionPanel2.Size = new System.Drawing.Size(639, 28);
+			this.accordionPanel2.Text = "Available Products";
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.Anchor = Wisej.Web.AnchorStyles.None;
+			this.radioButton5.Location = new System.Drawing.Point(406, 208);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(77, 23);
+			this.radioButton5.TabIndex = 14;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "Orlando";
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.Anchor = Wisej.Web.AnchorStyles.None;
+			this.radioButton6.Location = new System.Drawing.Point(406, 175);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(87, 23);
+			this.radioButton6.TabIndex = 13;
+			this.radioButton6.TabStop = true;
+			this.radioButton6.Text = "Las Vegas";
+			// 
+			// radioButton7
+			// 
+			this.radioButton7.Anchor = Wisej.Web.AnchorStyles.None;
+			this.radioButton7.Location = new System.Drawing.Point(406, 142);
+			this.radioButton7.Name = "radioButton7";
+			this.radioButton7.Size = new System.Drawing.Size(111, 23);
+			this.radioButton7.TabIndex = 12;
+			this.radioButton7.TabStop = true;
+			this.radioButton7.Text = "New York City";
+			// 
+			// radioButton8
+			// 
+			this.radioButton8.Anchor = Wisej.Web.AnchorStyles.None;
+			this.radioButton8.Location = new System.Drawing.Point(406, 109);
+			this.radioButton8.Name = "radioButton8";
+			this.radioButton8.Size = new System.Drawing.Size(84, 23);
+			this.radioButton8.TabIndex = 11;
+			this.radioButton8.TabStop = true;
+			this.radioButton8.Text = "Arlington";
+			// 
+			// button2
+			// 
+			this.button2.Anchor = Wisej.Web.AnchorStyles.None;
+			this.button2.Location = new System.Drawing.Point(122, 214);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(206, 37);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Submit";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Anchor = Wisej.Web.AnchorStyles.None;
+			this.numericUpDown1.LabelText = "Values";
+			this.numericUpDown1.Location = new System.Drawing.Point(122, 143);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(206, 53);
+			this.numericUpDown1.TabIndex = 9;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = Wisej.Web.AnchorStyles.None;
+			this.textBox1.LabelText = "Name";
+			this.textBox1.Location = new System.Drawing.Point(122, 72);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(206, 53);
+			this.textBox1.TabIndex = 8;
+			// 
+			// imageList1
+			// 
+			this.imageList1.Images.AddRange(new Wisej.Web.ImageListEntry[] {
+            imageListEntry1,
+            imageListEntry2,
+            imageListEntry3,
+            imageListEntry4,
+            imageListEntry5,
+            imageListEntry6,
+            imageListEntry7,
+            imageListEntry8,
+            imageListEntry9,
+            imageListEntry10});
+			// 
+			// treeView2
+			// 
+			this.treeView2.Dock = Wisej.Web.DockStyle.Fill;
+			this.treeView2.ImageIndex = 0;
+			this.treeView2.ImageList = this.imageList1;
+			this.treeView2.Location = new System.Drawing.Point(8, 8);
+			this.treeView2.Name = "treeView2";
+			treeNode1.ImageIndex = 8;
+			treeNode1.Name = "Node0";
+			treeNode2.ImageIndex = 0;
+			treeNode2.Name = "Node4";
+			treeNode2.SelectedImageKey = "0";
+			treeNode2.Text = "Apple";
+			treeNode3.ImageIndex = 1;
+			treeNode3.Name = "Node8";
+			treeNode3.SelectedImageIndex = 1;
+			treeNode3.Text = "Banana";
+			treeNode4.ImageIndex = 5;
+			treeNode4.Name = "Node9";
+			treeNode4.SelectedImageIndex = 5;
+			treeNode4.Text = "Orange";
+			treeNode1.Nodes.AddRange(new Wisej.Web.TreeNode[] {
+            treeNode2,
+            treeNode3,
+            treeNode4});
+			treeNode1.SelectedImageIndex = 8;
+			treeNode1.Text = "Fruits";
+			treeNode5.ImageIndex = 9;
+			treeNode5.Name = "Node1";
+			treeNode6.ImageIndex = 3;
+			treeNode6.Name = "Node6";
+			treeNode6.SelectedImageIndex = 3;
+			treeNode6.Text = "Cucumber";
+			treeNode7.ImageIndex = 2;
+			treeNode7.Name = "Node10";
+			treeNode7.SelectedImageIndex = 2;
+			treeNode7.Text = "Carrot";
+			treeNode5.Nodes.AddRange(new Wisej.Web.TreeNode[] {
+            treeNode6,
+            treeNode7});
+			treeNode5.SelectedImageIndex = 9;
+			treeNode5.Text = "Vegetables";
+			treeNode8.ImageIndex = 7;
+			treeNode8.Name = "Node2";
+			treeNode9.ImageIndex = 4;
+			treeNode9.Name = "Node7";
+			treeNode9.SelectedImageIndex = 4;
+			treeNode9.Text = "Milk";
+			treeNode10.ImageIndex = 6;
+			treeNode10.Name = "Node1";
+			treeNode10.SelectedImageIndex = 6;
+			treeNode10.Text = "Yogurt";
+			treeNode8.Nodes.AddRange(new Wisej.Web.TreeNode[] {
+            treeNode9,
+            treeNode10});
+			treeNode8.SelectedImageIndex = 7;
+			treeNode8.Text = "Dairy";
+			this.treeView2.Nodes.AddRange(new Wisej.Web.TreeNode[] {
+            treeNode1,
+            treeNode5,
+            treeNode8});
+			this.treeView2.RightClickSelection = true;
+			this.treeView2.SelectedImageIndex = 0;
+			this.treeView2.Size = new System.Drawing.Size(623, 307);
+			this.treeView2.TabIndex = 2;
+			// 
+			// chkCollapsible
+			// 
+			this.chkCollapsible.Anchor = Wisej.Web.AnchorStyles.None;
+			this.chkCollapsible.Location = new System.Drawing.Point(286, 204);
+			this.chkCollapsible.Name = "chkCollapsible";
+			this.chkCollapsible.Size = new System.Drawing.Size(93, 23);
+			this.chkCollapsible.TabIndex = 1;
+			this.chkCollapsible.Text = "Collapsible";
+			this.chkCollapsible.CheckedChanged += new System.EventHandler(this.chkCollapsible_CheckedChanged);
+			// 
+			// chkSelectedOnTop
+			// 
+			this.chkSelectedOnTop.Anchor = Wisej.Web.AnchorStyles.None;
+			this.chkSelectedOnTop.Location = new System.Drawing.Point(286, 247);
+			this.chkSelectedOnTop.Name = "chkSelectedOnTop";
+			this.chkSelectedOnTop.Size = new System.Drawing.Size(123, 23);
+			this.chkSelectedOnTop.TabIndex = 2;
+			this.chkSelectedOnTop.Text = "Selected on Top";
+			this.chkSelectedOnTop.CheckedChanged += new System.EventHandler(this.chkSelectedOnTop_CheckedChanged);
+			// 
+			// Features
+			// 
+			this.Controls.Add(this.chkSelectedOnTop);
+			this.Controls.Add(this.chkCollapsible);
+			this.Controls.Add(this.accordion1);
+			this.Name = "Features";
+			this.Load += new System.EventHandler(this.Features_Load);
+			((System.ComponentModel.ISupportInitialize)(this.accordion1)).EndInit();
+			this.accordion1.ResumeLayout(false);
+			this.accordionPanel1.ResumeLayout(false);
+			this.accordionPanel1.PerformLayout();
+			this.accordionPanel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Web.Accordion accordion1;
+		private Web.AccordionPanel accordionPanel1;
+		private Web.AccordionPanel accordionPanel2;
+		private Web.RadioButton radioButton5;
+		private Web.RadioButton radioButton6;
+		private Web.RadioButton radioButton7;
+		private Web.RadioButton radioButton8;
+		private Web.Button button2;
+		private Web.NumericUpDown numericUpDown1;
+		private Web.TextBox textBox1;
+		private Web.ImageList imageList1;
+		private Web.TreeView treeView2;
+		private Web.CheckBox chkCollapsible;
+		private Web.CheckBox chkSelectedOnTop;
+	}
+}
---
+++ Demos/Editors/Wisej.DemoBrowser.TagTextBox/Features.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Wisej.DemoBrowser.TagTextBox
+{
+	partial class Features
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tagDefault = new Wisej.Web.TagTextBox();
+			this.tagCustomRender = new Wisej.Web.TagTextBox();
+			this.tagAutoComplete = new Wisej.Web.TagTextBox();
+			this.SuspendLayout();
+			// 
+			// tagDefault
+			// 
+			this.tagDefault.Anchor = Wisej.Web.AnchorStyles.None;
+			this.tagDefault.LabelText = "Default TagTextBox";
+			this.tagDefault.Location = new System.Drawing.Point(281, 42);
+			this.tagDefault.Name = "tagDefault";
+			this.tagDefault.Size = new System.Drawing.Size(253, 51);
+			this.tagDefault.TabIndex = 0;
+			this.tagDefault.TagRemoved += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Removed);
+			this.tagDefault.TagRejected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Rejected);
+			this.tagDefault.TagSelected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Selected);
+			// 
+			// tagCustomRender
+			// 
+			this.tagCustomRender.Anchor = Wisej.Web.AnchorStyles.None;
+			this.tagCustomRender.LabelText = "Custom Tag Rendering";
+			this.tagCustomRender.Location = new System.Drawing.Point(600, 42);
+			this.tagCustomRender.Name = "tagCustomRender";
+			this.tagCustomRender.Size = new System.Drawing.Size(253, 51);
+			this.tagCustomRender.TabIndex = 1;
+			this.tagCustomRender.TagRemoved += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Removed);
+			this.tagCustomRender.TagRejected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Rejected);
+			this.tagCustomRender.TagSelected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Selected);
+			this.tagCustomRender.TagRender += new Wisej.Web.TagTextBoxRenderEventHandler(this.tagTextBox2_TagRender);
+			// 
+			// tagAutoComplete
+			// 
+			this.tagAutoComplete.Anchor = Wisej.Web.AnchorStyles.None;
+			this.tagAutoComplete.AutoCompleteList = new string[] {
+        "Alabama",
+        "Alaska",
+        "American Samoa",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "District of Columbia",
+        "Florida",
+        "Georgia",
+        "Guam",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Minor Outlying Islands",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Northern Mariana Islands",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Puerto Rico",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "U.S. Virgin Islands",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming"};
+			this.tagAutoComplete.LabelText = "Auto Complete TagTextBox";
+			this.tagAutoComplete.Location = new System.Drawing.Point(889, 42);
+			this.tagAutoComplete.Name = "tagAutoComplete";
+			this.tagAutoComplete.Size = new System.Drawing.Size(253, 51);
+			this.tagAutoComplete.TabIndex = 2;
+			this.tagAutoComplete.Text = "Alabama,\r\nAlaska,\r\nAmerican Samoa,\r\nArizona,";
+			this.tagAutoComplete.TagAdded += new Wisej.Web.TagTextBoxEventHandler(this.tagAutoComplete_TagAdded);
+			this.tagAutoComplete.TagRemoved += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Removed);
+			this.tagAutoComplete.TagRejected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Rejected);
+			this.tagAutoComplete.TagSelected += new Wisej.Web.TagTextBoxEventHandler(this.Tag_Selected);
+			// 
+			// Features
+			// 
+			this.Controls.Add(this.tagAutoComplete);
+			this.Controls.Add(this.tagCustomRender);
+			this.Controls.Add(this.tagDefault);
+			this.Name = "Features";
+			this.Load += new System.EventHandler(this.Features_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Web.TagTextBox tagDefault;
+		private Web.TagTextBox tagCustomRender;
+		private Web.TagTextBox tagAutoComplete;
+	}
+}
