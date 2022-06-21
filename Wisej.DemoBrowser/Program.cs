6008097c429643c8f1310a08f022a58d05d95e4b
LRufenacht <llrufenacht@gmail.com>
2022-06-21 12:14:09 -0400

    Removed font check. Updated comments.

--- Wisej.DemoBrowser/Program.cs
+++ Wisej.DemoBrowser/Program.cs
@@ -10,60 +10,9 @@ namespace Wisej.DemoBrowser
 		/// </summary>
 		static void Main()
 		{
-			// check font installed?
-			if (!CheckOpenSansInstalled())
-				Web.MessageBox.Show("Install <a target='_blank' href='https://fonts.google.com/specimen/Open+Sans'>Open Sans</a> for an optimal experience.<br>", allowHtml: true);
-
 			Application.MainPage = new MainView();
 			Application.SessionTimeout += Application_SessionTimeout;
 		}
-
-		private static bool CheckOpenSansInstalled()
-        {
-            //if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.OSX))
-            //{
-            //    //WIP
-            //    return true;
-            //}
-
-            //if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
-            //{
-                var fontsCollection = new System.Drawing.Text.InstalledFontCollection();
-                foreach (var fontFamily in fontsCollection.Families)
-                    if (fontFamily.Name == "Open Sans")
-                        return true;
-
-                return false;
-            //}
-
-            //if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux))
-            //{
-            //    string command = "fc-list | grep –I OpenSans";
-            //    string result = "";
-            //    using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
-            //    {
-            //        proc.StartInfo.FileName = "/bin/bash";
-            //        proc.StartInfo.Arguments = "-c \" " + command + " \"";
-            //        proc.StartInfo.UseShellExecute = false;
-            //        proc.StartInfo.RedirectStandardOutput = true;
-            //        proc.StartInfo.RedirectStandardError = true;
-            //        proc.Start();
-
-            //        result += proc.StandardOutput.ReadToEnd();
-            //        result += proc.StandardError.ReadToEnd();
-
-            //        proc.WaitForExit();
-
-            //    }
-            //    if (!string.IsNullOrEmpty(result) && result.ToLower().Contains("opensans"))
-            //    {
-            //        return true;
-            //    }
-            //    return false;
-            //}
-
-            //return false;
-        }
 
 		private static void Application_SessionTimeout(object sender, System.ComponentModel.HandledEventArgs e)
 		{
