181f398119b65e92a20f736ff93cf7a02d23d7d0
Kevin Bush <keevbush@hotmail.com>
2022-08-21 20:06:50 +0200

    Fix https://github.com/iceteagroup/wisej-demobrowser/issues/125

--- Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Syncfusion2.Demo/Component/Diagram.Designer.cs
+++ Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Syncfusion2.Demo/Component/Diagram.Designer.cs
@@ -28,52 +28,90 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.diagram1 = new Wisej.Web.Ext.Syncfusion2.Diagram();
-			this.groupBox1.SuspendLayout();
-			this.panel.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// linkDocs
-			// 
-			this.linkDocs.Text = "https://ej2.syncfusion.com/javascript/documentation/diagram/es5-getting-started/";
-			// 
-			// linkApi
-			// 
-			this.linkApi.Text = "https://ej2.syncfusion.com/javascript/documentation/api/diagram/";
-			// 
-			// panel
-			// 
-			this.panel.Controls.Add(this.diagram1);
-			// 
-			// linkDemo
-			// 
-			this.linkDemo.Text = "https://ej2.syncfusion.com/demos/#/bootstrap5/diagram/default-functionalities.htm" +
+            this.components = new System.ComponentModel.Container();
+            this.diagram1 = new Wisej.Web.Ext.Syncfusion2.Diagram();
+            this.buttonExportToPNG = new Wisej.Web.Button();
+            this.colorDialog1 = new Wisej.Web.ColorDialog(this.components);
+            this.buttonChangeLineColor = new Wisej.Web.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // linkDocs
+            // 
+            this.linkDocs.Text = "https://ej2.syncfusion.com/javascript/documentation/diagram/es5-getting-started/";
+            // 
+            // linkApi
+            // 
+            this.linkApi.Text = "https://ej2.syncfusion.com/javascript/documentation/api/diagram/";
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.diagram1);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonExportToPNG);
+            this.flowLayoutPanel1.Controls.Add(this.buttonChangeLineColor);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(16, 46);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(218, 469);
+            // 
+            // linkDemo
+            // 
+            this.linkDemo.Text = "https://ej2.syncfusion.com/demos/#/bootstrap5/diagram/default-functionalities.htm" +
     "l";
-			// 
-			// diagram1
-			// 
-			this.diagram1.Dock = Wisej.Web.DockStyle.Fill;
-			this.diagram1.Location = new System.Drawing.Point(8, 8);
-			this.diagram1.Name = "diagram1";
-			this.diagram1.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{\"nodes\":[{\"name\":\"Start\",\"offsetX\":300,\"offsetY\":50,\"width\":140,\"height\":50,\"ann" +
+            // 
+            // diagram1
+            // 
+            this.diagram1.Dock = Wisej.Web.DockStyle.Fill;
+            this.diagram1.Location = new System.Drawing.Point(8, 8);
+            this.diagram1.Name = "diagram1";
+            this.diagram1.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{\"nodes\":[{\"name\":\"Start\",\"offsetX\":300,\"offsetY\":50,\"width\":140,\"height\":50,\"ann" +
         "otations\":[{\"id\":\"label1\",\"content\":\"Start\"}],\"shape\":{\"type\":\"Flow\",\"shape\":\"Te" +
         "rminator\"}}]}")));
-			this.diagram1.Size = new System.Drawing.Size(872, 560);
-			this.diagram1.TabIndex = 0;
-			this.diagram1.Text = "diagram1";
-			// 
-			// Diagram
-			// 
-			this.Name = "Diagram";
-			this.Load += new System.EventHandler(this.Diagram_Load);
-			this.groupBox1.ResumeLayout(false);
-			this.panel.ResumeLayout(false);
-			this.ResumeLayout(false);
+            this.diagram1.Size = new System.Drawing.Size(872, 560);
+            this.diagram1.TabIndex = 0;
+            this.diagram1.Text = "diagram1";
+            // 
+            // buttonExportToPNG
+            // 
+            this.buttonExportToPNG.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.buttonExportToPNG.Location = new System.Drawing.Point(3, 3);
+            this.buttonExportToPNG.Name = "buttonExportToPNG";
+            this.buttonExportToPNG.Size = new System.Drawing.Size(212, 27);
+            this.buttonExportToPNG.TabIndex = 18;
+            this.buttonExportToPNG.Text = "Export to PNG";
+            this.buttonExportToPNG.Click += new System.EventHandler(this.buttonExportToPNG_Click);
+            // 
+            // buttonChangeLineColor
+            // 
+            this.buttonChangeLineColor.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.buttonChangeLineColor.Location = new System.Drawing.Point(3, 36);
+            this.buttonChangeLineColor.Name = "buttonChangeLineColor";
+            this.buttonChangeLineColor.Size = new System.Drawing.Size(212, 27);
+            this.buttonChangeLineColor.TabIndex = 18;
+            this.buttonChangeLineColor.Text = "Change line color";
+            this.buttonChangeLineColor.Click += new System.EventHandler(this.buttonChangeLineColor_Click);
+            // 
+            // Diagram
+            // 
+            this.Name = "Diagram";
+            this.Load += new System.EventHandler(this.Diagram_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private Syncfusion2.Diagram diagram1;
-	}
+        private Button buttonExportToPNG;
+        private Button buttonChangeLineColor;
+        private ColorDialog colorDialog1;
+    }
 }
--- Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Syncfusion2.Demo/Component/Diagram.cs
+++ Wisej.DemoBrowser.Premium/Wisej.Web.Ext.Syncfusion2.Demo/Component/Diagram.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using Wisej.Web;
+using System.Text.RegularExpressions;
 
 namespace Wisej.Web.Ext.Syncfusion2.Demo.Component
 {
@@ -18,6 +20,8 @@ namespace Wisej.Web.Ext.Syncfusion2.Demo.Component
 			{
 				new
 				{
+					
+					id = "Start",
 					name = "Start",
 					offsetX = 300, offsetY = 50, width = 140, height = 50, annotations = new object[]
 					{
@@ -32,7 +36,35 @@ namespace Wisej.Web.Ext.Syncfusion2.Demo.Component
 						type = "Flow",
 						shape = "Terminator"
 					}
+				},
+				new
+				{
+					id = "End",
+					name = "End",
+					offsetX = 300, offsetY = 250, width = 140, height = 50, annotations = new object[]
+					{
+						new
+						{
+							id = "label1",
+							content = "End"
+						}
+					},
+					shape = new
+					{
+						type = "Flow",
+						shape = "Terminator"
+					}
 				}
+			};
+
+			this.diagram1.Options.connectors = new object[]
+			{
+				new
+                {
+					id = "connector_1",
+					sourceID = "Start",
+					targetID = "End"
+                }
 			};
 		}
 
@@ -44,5 +76,53 @@ namespace Wisej.Web.Ext.Syncfusion2.Demo.Component
 
 			Application.Play(MessageBoxIcon.Information);
 		}
-	}
+
+
+        private async void buttonExportToPNG_Click(object sender, EventArgs e)
+        {
+			var data = await this.diagram1.Instance.exportDiagramAsync(new
+			{
+				mode = "Download",
+			});
+
+			string fileBase64 = ((string)data).Replace("data:image/png;base64,", ""); // ((string)data).Split(new [] {"xx"});
+			using (MemoryStream file = new MemoryStream(Convert.FromBase64String(fileBase64)))
+			{
+				Application.Download(file, "ExportFile.png", (image) =>
+				{
+					AlertBox.Show("File downloaded");
+				});
+
+			}
+		}
+
+        private async void buttonChangeLineColor_Click(object sender, EventArgs e)
+        {
+			var result = await this.colorDialog1.ShowDialogAsync();
+
+			if(result == DialogResult.OK)
+            {
+				string color = $"#{this.colorDialog1.Color.R.ToString("X2")}{this.colorDialog1.Color.G.ToString("X2")}{this.colorDialog1.Color.B.ToString("X2")}";
+
+				this.diagram1.Options.snapSettings = new
+				{
+					horizontalGridlines = new
+                    {
+						// Sets the line color of gridlines
+						lineColor = color,
+						// Defines the lineDashArray of gridlines
+						lineDashArray = "2 2"
+					},
+					verticalGridlines = new
+					{
+						// Sets the line color of gridlines
+						lineColor = color,
+						// Defines the lineDashArray of gridlines
+						lineDashArray = "2 2"
+					}
+				};
+
+			}
+        }
+    }
 }
