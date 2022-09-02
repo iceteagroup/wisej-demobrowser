841efa79cca549bf9ee38f911eefe6247844367f
Kevin Bush <keevbush@hotmail.com>
2022-09-03 00:06:16 +0200

    Finish Wisej.DemoBrowser.TextBox -> Label

--- Demos/Editors/Wisej.DemoBrowser.TextBox/Labels.Designer.cs
+++ Demos/Editors/Wisej.DemoBrowser.TextBox/Labels.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Labels));
             this.textBoxInside = new Wisej.Web.TextBox();
             this.textBox5 = new Wisej.Web.TextBox();
             this.textBox4 = new Wisej.Web.TextBox();
@@ -49,7 +50,9 @@
             this.textBoxInside.LabelText = "Inside";
             this.textBoxInside.Location = new System.Drawing.Point(391, 116);
             this.textBoxInside.Name = "textBoxInside";
-            this.textBoxInside.Size = new System.Drawing.Size(164, 48);
+            this.textBoxInside.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBoxInside.ResponsiveProfiles"))));
+            this.textBoxInside.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBoxInside.ResponsiveProfiles1"))));
+            this.textBoxInside.Size = new System.Drawing.Size(164, 37);
             this.textBoxInside.TabIndex = 9;
             // 
             // textBox5
@@ -59,7 +62,9 @@
             this.textBox5.LabelText = "Bottom Left";
             this.textBox5.Location = new System.Drawing.Point(6, 210);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(164, 53);
+            this.textBox5.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox5.ResponsiveProfiles"))));
+            this.textBox5.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox5.ResponsiveProfiles1"))));
+            this.textBox5.Size = new System.Drawing.Size(164, 42);
             this.textBox5.TabIndex = 12;
             // 
             // textBox4
@@ -69,7 +74,9 @@
             this.textBox4.LabelText = "Left";
             this.textBox4.Location = new System.Drawing.Point(6, 121);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(164, 30);
+            this.textBox4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox4.ResponsiveProfiles"))));
+            this.textBox4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox4.ResponsiveProfiles1"))));
+            this.textBox4.Size = new System.Drawing.Size(164, 22);
             this.textBox4.TabIndex = 11;
             // 
             // textBox3
@@ -79,7 +86,9 @@
             this.textBox3.LabelText = "Right";
             this.textBox3.Location = new System.Drawing.Point(198, 121);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(164, 30);
+            this.textBox3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox3.ResponsiveProfiles"))));
+            this.textBox3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox3.ResponsiveProfiles1"))));
+            this.textBox3.Size = new System.Drawing.Size(164, 22);
             this.textBox3.TabIndex = 10;
             // 
             // textBoxAbove
@@ -88,7 +97,9 @@
             this.textBoxAbove.LabelText = "Default";
             this.textBoxAbove.Location = new System.Drawing.Point(6, 9);
             this.textBoxAbove.Name = "textBoxAbove";
-            this.textBoxAbove.Size = new System.Drawing.Size(164, 53);
+            this.textBoxAbove.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBoxAbove.ResponsiveProfiles"))));
+            this.textBoxAbove.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBoxAbove.ResponsiveProfiles1"))));
+            this.textBoxAbove.Size = new System.Drawing.Size(164, 42);
             this.textBoxAbove.TabIndex = 8;
             // 
             // textBox1
@@ -98,7 +109,9 @@
             this.textBox1.LabelText = "Center-Align";
             this.textBox1.Location = new System.Drawing.Point(198, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 53);
+            this.textBox1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox1.ResponsiveProfiles"))));
+            this.textBox1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox1.ResponsiveProfiles1"))));
+            this.textBox1.Size = new System.Drawing.Size(164, 42);
             this.textBox1.TabIndex = 13;
             // 
             // textBox2
@@ -108,7 +121,9 @@
             this.textBox2.LabelText = "Right-Align";
             this.textBox2.Location = new System.Drawing.Point(391, 9);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 53);
+            this.textBox2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox2.ResponsiveProfiles"))));
+            this.textBox2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox2.ResponsiveProfiles1"))));
+            this.textBox2.Size = new System.Drawing.Size(164, 42);
             this.textBox2.TabIndex = 14;
             // 
             // textBox6
@@ -118,7 +133,9 @@
             this.textBox6.LabelText = "Custom Padding";
             this.textBox6.Location = new System.Drawing.Point(582, 9);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(164, 53);
+            this.textBox6.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox6.ResponsiveProfiles"))));
+            this.textBox6.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox6.ResponsiveProfiles1"))));
+            this.textBox6.Size = new System.Drawing.Size(164, 42);
             this.textBox6.TabIndex = 15;
             // 
             // textBox7
@@ -128,7 +145,9 @@
             this.textBox7.LabelText = "Custom Color";
             this.textBox7.Location = new System.Drawing.Point(582, 98);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(164, 53);
+            this.textBox7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox7.ResponsiveProfiles"))));
+            this.textBox7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox7.ResponsiveProfiles1"))));
+            this.textBox7.Size = new System.Drawing.Size(164, 42);
             this.textBox7.TabIndex = 16;
             // 
             // textBox8
@@ -138,7 +157,9 @@
             this.textBox8.LabelText = "<b>Label</b> with <i>HTML</i>";
             this.textBox8.Location = new System.Drawing.Point(582, 170);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(164, 70);
+            this.textBox8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox8.ResponsiveProfiles"))));
+            this.textBox8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox8.ResponsiveProfiles1"))));
+            this.textBox8.Size = new System.Drawing.Size(164, 57);
             this.textBox8.TabIndex = 17;
             // 
             // textBox9
@@ -149,7 +170,9 @@
             this.textBox9.LabelText = "Bottom Center";
             this.textBox9.Location = new System.Drawing.Point(198, 210);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(164, 53);
+            this.textBox9.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox9.ResponsiveProfiles"))));
+            this.textBox9.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox9.ResponsiveProfiles1"))));
+            this.textBox9.Size = new System.Drawing.Size(164, 42);
             this.textBox9.TabIndex = 18;
             // 
             // textBox10
@@ -160,7 +183,9 @@
             this.textBox10.LabelText = "Bottom Right";
             this.textBox10.Location = new System.Drawing.Point(391, 210);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(164, 53);
+            this.textBox10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox10.ResponsiveProfiles"))));
+            this.textBox10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("textBox10.ResponsiveProfiles1"))));
+            this.textBox10.Size = new System.Drawing.Size(164, 42);
             this.textBox10.TabIndex = 19;
             // 
             // Labels
@@ -179,6 +204,8 @@
             this.Controls.Add(this.textBoxAbove);
             this.MinimumSize = new System.Drawing.Size(752, 273);
             this.Name = "Labels";
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles"))));
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles1"))));
             this.Size = new System.Drawing.Size(752, 273);
             this.ResumeLayout(false);
             this.PerformLayout();
