5c1b263a222c93540f20d34b23e3a2233f350893
Kevin Bush <keevbush@hotmail.com>
2022-09-04 13:34:21 +0200

    Finish Wisej.DemoBrowser.Button

--- Demos/Buttons/Wisej.DemoBrowser.Button/Features.Designer.cs
+++ Demos/Buttons/Wisej.DemoBrowser.Button/Features.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Features));
             this.buttonAlertBox = new Wisej.Web.Button();
             this.button2 = new Wisej.Web.Button();
             this.button4 = new Wisej.Web.Button();
@@ -61,6 +62,8 @@
             this.buttonAlertBox.Location = new System.Drawing.Point(546, 190);
             this.buttonAlertBox.Margin = new Wisej.Web.Padding(16);
             this.buttonAlertBox.Name = "buttonAlertBox";
+            this.buttonAlertBox.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonAlertBox.ResponsiveProfiles"))));
+            this.buttonAlertBox.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonAlertBox.ResponsiveProfiles1"))));
             this.buttonAlertBox.Size = new System.Drawing.Size(137, 37);
             this.buttonAlertBox.TabIndex = 0;
             this.buttonAlertBox.Text = "Alert";
@@ -73,6 +76,8 @@
             this.button2.Location = new System.Drawing.Point(289, 186);
             this.button2.Margin = new Wisej.Web.Padding(16);
             this.button2.Name = "button2";
+            this.button2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button2.ResponsiveProfiles"))));
+            this.button2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button2.ResponsiveProfiles1"))));
             this.button2.Size = new System.Drawing.Size(225, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "Image Before Text";
@@ -84,6 +89,8 @@
             this.button4.Location = new System.Drawing.Point(32, 186);
             this.button4.Margin = new Wisej.Web.Padding(16);
             this.button4.Name = "button4";
+            this.button4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button4.ResponsiveProfiles"))));
+            this.button4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button4.ResponsiveProfiles1"))));
             this.button4.Size = new System.Drawing.Size(225, 45);
             this.button4.TabIndex = 3;
             this.button4.Text = "Text Before Image";
@@ -95,6 +102,8 @@
             this.buttonMessageBox.Location = new System.Drawing.Point(972, 113);
             this.buttonMessageBox.Margin = new Wisej.Web.Padding(16);
             this.buttonMessageBox.Name = "buttonMessageBox";
+            this.buttonMessageBox.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonMessageBox.ResponsiveProfiles"))));
+            this.buttonMessageBox.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonMessageBox.ResponsiveProfiles1"))));
             this.buttonMessageBox.Size = new System.Drawing.Size(137, 37);
             this.buttonMessageBox.TabIndex = 4;
             this.buttonMessageBox.Text = "Message Box";
@@ -106,6 +115,8 @@
             this.buttonToast.Location = new System.Drawing.Point(803, 113);
             this.buttonToast.Margin = new Wisej.Web.Padding(16);
             this.buttonToast.Name = "buttonToast";
+            this.buttonToast.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonToast.ResponsiveProfiles"))));
+            this.buttonToast.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonToast.ResponsiveProfiles1"))));
             this.buttonToast.Size = new System.Drawing.Size(137, 37);
             this.buttonToast.TabIndex = 5;
             this.buttonToast.Text = "Toast";
@@ -118,6 +129,8 @@
             this.button3.Location = new System.Drawing.Point(546, 109);
             this.button3.Margin = new Wisej.Web.Padding(16);
             this.button3.Name = "button3";
+            this.button3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button3.ResponsiveProfiles"))));
+            this.button3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button3.ResponsiveProfiles1"))));
             this.button3.Size = new System.Drawing.Size(225, 45);
             this.button3.TabIndex = 6;
             this.button3.Text = "Overlay";
@@ -130,6 +143,8 @@
             this.button7.Location = new System.Drawing.Point(289, 109);
             this.button7.Margin = new Wisej.Web.Padding(16);
             this.button7.Name = "button7";
+            this.button7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button7.ResponsiveProfiles"))));
+            this.button7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button7.ResponsiveProfiles1"))));
             this.button7.Size = new System.Drawing.Size(225, 45);
             this.button7.TabIndex = 7;
             this.button7.Text = "Image Above Text";
@@ -142,6 +157,8 @@
             this.button8.Location = new System.Drawing.Point(32, 109);
             this.button8.Margin = new Wisej.Web.Padding(16);
             this.button8.Name = "button8";
+            this.button8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button8.ResponsiveProfiles"))));
+            this.button8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button8.ResponsiveProfiles1"))));
             this.button8.Size = new System.Drawing.Size(225, 45);
             this.button8.TabIndex = 8;
             this.button8.Text = "Text Above Image";
@@ -153,6 +170,8 @@
             this.buttonNativeAlert.Location = new System.Drawing.Point(1060, 36);
             this.buttonNativeAlert.Margin = new Wisej.Web.Padding(16);
             this.buttonNativeAlert.Name = "buttonNativeAlert";
+            this.buttonNativeAlert.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonNativeAlert.ResponsiveProfiles"))));
+            this.buttonNativeAlert.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonNativeAlert.ResponsiveProfiles1"))));
             this.buttonNativeAlert.Size = new System.Drawing.Size(137, 37);
             this.buttonNativeAlert.TabIndex = 9;
             this.buttonNativeAlert.Text = "Native Alert";
@@ -165,6 +184,8 @@
             this.button10.Location = new System.Drawing.Point(803, 32);
             this.button10.Margin = new Wisej.Web.Padding(16);
             this.button10.Name = "button10";
+            this.button10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button10.ResponsiveProfiles"))));
+            this.button10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("button10.ResponsiveProfiles1"))));
             this.button10.Size = new System.Drawing.Size(225, 45);
             this.button10.TabIndex = 10;
             this.button10.Text = "Button <b style=\'color: red\'>with</b> <i>HTML<i>";
@@ -181,6 +202,8 @@
             this.menuItem4,
             this.menuItem5});
             this.splitButton1.Name = "splitButton1";
+            this.splitButton1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("splitButton1.ResponsiveProfiles"))));
+            this.splitButton1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("splitButton1.ResponsiveProfiles1"))));
             this.splitButton1.Size = new System.Drawing.Size(225, 45);
             this.splitButton1.TabIndex = 11;
             this.splitButton1.Text = "Split Button";
@@ -228,6 +251,8 @@
             this.menuItem10});
             this.splitButton2.Name = "splitButton2";
             this.splitButton2.Orientation = Wisej.Web.Orientation.Vertical;
+            this.splitButton2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("splitButton2.ResponsiveProfiles"))));
+            this.splitButton2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("splitButton2.ResponsiveProfiles1"))));
             this.splitButton2.Size = new System.Drawing.Size(225, 45);
             this.splitButton2.TabIndex = 12;
             this.splitButton2.Text = "Vertical Split Button";
@@ -269,6 +294,7 @@
             this.buttonRepeat.Margin = new Wisej.Web.Padding(16);
             this.buttonRepeat.Name = "buttonRepeat";
             this.buttonRepeat.Repeat = true;
+            this.buttonRepeat.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("buttonRepeat.ResponsiveProfiles"))));
             this.buttonRepeat.Size = new System.Drawing.Size(225, 45);
             this.buttonRepeat.TabIndex = 13;
             this.buttonRepeat.Text = "Repeat (0)";
@@ -295,15 +321,18 @@
             this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 74);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Padding = new Wisej.Web.Padding(16);
+            this.flowLayoutPanel1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("flowLayoutPanel1.ResponsiveProfiles"))));
+            this.flowLayoutPanel1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("flowLayoutPanel1.ResponsiveProfiles1"))));
             this.flowLayoutPanel1.Size = new System.Drawing.Size(1383, 263);
             this.flowLayoutPanel1.TabIndex = 14;
             // 
             // Features
             // 
             this.AutoScroll = true;
-            this.AutoScrollMargin = new System.Drawing.Size(0, 20);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Features";
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles"))));
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles1"))));
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
