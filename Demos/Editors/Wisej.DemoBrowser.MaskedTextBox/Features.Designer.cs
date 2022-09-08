b5bf63180ea6599127a2b6d96c3b13b0bbbe0a38
Kevin Bush <keevbush@hotmail.com>
2022-09-08 20:48:16 +0200

    Finish MaskedTextBox and MonthCalendar

--- Demos/Editors/Wisej.DemoBrowser.MaskedTextBox/Features.Designer.cs
+++ Demos/Editors/Wisej.DemoBrowser.MaskedTextBox/Features.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Features));
             this.maskedTextBox1 = new Wisej.Web.MaskedTextBox();
             this.maskedTextBox2 = new Wisej.Web.MaskedTextBox();
             this.maskedTextBox3 = new Wisej.Web.MaskedTextBox();
@@ -47,7 +48,9 @@
             this.maskedTextBox1.LabelText = "Watermark";
             this.maskedTextBox1.Location = new System.Drawing.Point(295, 126);
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox1.ResponsiveProfiles"))));
+            this.maskedTextBox1.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox1.ResponsiveProfiles1"))));
+            this.maskedTextBox1.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox1.TabIndex = 0;
             this.maskedTextBox1.Watermark = "Enter some text...";
             // 
@@ -58,7 +61,9 @@
             this.maskedTextBox2.LabelText = "BorderStyle Dotted";
             this.maskedTextBox2.Location = new System.Drawing.Point(877, 90);
             this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox2.ResponsiveProfiles"))));
+            this.maskedTextBox2.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox2.ResponsiveProfiles1"))));
+            this.maskedTextBox2.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox2.TabIndex = 7;
             // 
             // maskedTextBox3
@@ -68,7 +73,9 @@
             this.maskedTextBox3.LabelText = "BorderStyle Dashed";
             this.maskedTextBox3.Location = new System.Drawing.Point(877, 171);
             this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox3.ResponsiveProfiles"))));
+            this.maskedTextBox3.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox3.ResponsiveProfiles1"))));
+            this.maskedTextBox3.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox3.TabIndex = 8;
             // 
             // maskedTextBox4
@@ -78,7 +85,9 @@
             this.maskedTextBox4.LabelText = "BorderStyle Double";
             this.maskedTextBox4.Location = new System.Drawing.Point(877, 252);
             this.maskedTextBox4.Name = "maskedTextBox4";
-            this.maskedTextBox4.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox4.ResponsiveProfiles"))));
+            this.maskedTextBox4.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox4.ResponsiveProfiles1"))));
+            this.maskedTextBox4.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox4.TabIndex = 9;
             // 
             // maskedTextBox5
@@ -88,7 +97,9 @@
             this.maskedTextBox5.LabelText = "BorderStyle None";
             this.maskedTextBox5.Location = new System.Drawing.Point(876, 332);
             this.maskedTextBox5.Name = "maskedTextBox5";
-            this.maskedTextBox5.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox5.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox5.ResponsiveProfiles"))));
+            this.maskedTextBox5.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox5.ResponsiveProfiles1"))));
+            this.maskedTextBox5.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox5.TabIndex = 10;
             this.maskedTextBox5.Text = "Hello, World!";
             // 
@@ -99,7 +110,9 @@
             this.maskedTextBox6.Location = new System.Drawing.Point(295, 210);
             this.maskedTextBox6.Name = "maskedTextBox6";
             this.maskedTextBox6.ReadOnly = true;
-            this.maskedTextBox6.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox6.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox6.ResponsiveProfiles"))));
+            this.maskedTextBox6.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox6.ResponsiveProfiles1"))));
+            this.maskedTextBox6.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox6.TabIndex = 1;
             this.maskedTextBox6.Text = "Hello, World!";
             // 
@@ -109,8 +122,10 @@
             this.maskedTextBox7.LabelText = "Select on Enter";
             this.maskedTextBox7.Location = new System.Drawing.Point(295, 294);
             this.maskedTextBox7.Name = "maskedTextBox7";
+            this.maskedTextBox7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox7.ResponsiveProfiles"))));
+            this.maskedTextBox7.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox7.ResponsiveProfiles1"))));
             this.maskedTextBox7.SelectOnEnter = true;
-            this.maskedTextBox7.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox7.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox7.TabIndex = 2;
             this.maskedTextBox7.Text = "Hello, World!";
             // 
@@ -121,7 +136,9 @@
             this.maskedTextBox8.Location = new System.Drawing.Point(586, 91);
             this.maskedTextBox8.Mask = "(999) 000-0000";
             this.maskedTextBox8.Name = "maskedTextBox8";
-            this.maskedTextBox8.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox8.ResponsiveProfiles"))));
+            this.maskedTextBox8.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox8.ResponsiveProfiles1"))));
+            this.maskedTextBox8.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox8.TabIndex = 3;
             this.maskedTextBox8.Watermark = "Enter some text...";
             // 
@@ -132,7 +149,9 @@
             this.maskedTextBox9.Location = new System.Drawing.Point(586, 171);
             this.maskedTextBox9.Mask = "00/00/0000";
             this.maskedTextBox9.Name = "maskedTextBox9";
-            this.maskedTextBox9.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox9.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox9.ResponsiveProfiles"))));
+            this.maskedTextBox9.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox9.ResponsiveProfiles1"))));
+            this.maskedTextBox9.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox9.TabIndex = 4;
             this.maskedTextBox9.ValidatingType = typeof(System.DateTime);
             this.maskedTextBox9.Watermark = "Enter some text...";
@@ -144,7 +163,9 @@
             this.maskedTextBox10.Location = new System.Drawing.Point(586, 251);
             this.maskedTextBox10.Mask = "000-00-0000";
             this.maskedTextBox10.Name = "maskedTextBox10";
-            this.maskedTextBox10.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox10.ResponsiveProfiles"))));
+            this.maskedTextBox10.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox10.ResponsiveProfiles1"))));
+            this.maskedTextBox10.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox10.TabIndex = 5;
             this.maskedTextBox10.ValidatingType = typeof(System.DateTime);
             this.maskedTextBox10.Watermark = "Enter some text...";
@@ -156,7 +177,9 @@
             this.maskedTextBox11.Location = new System.Drawing.Point(586, 331);
             this.maskedTextBox11.Mask = "00000";
             this.maskedTextBox11.Name = "maskedTextBox11";
-            this.maskedTextBox11.Size = new System.Drawing.Size(250, 53);
+            this.maskedTextBox11.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox11.ResponsiveProfiles"))));
+            this.maskedTextBox11.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("maskedTextBox11.ResponsiveProfiles1"))));
+            this.maskedTextBox11.Size = new System.Drawing.Size(250, 42);
             this.maskedTextBox11.TabIndex = 6;
             this.maskedTextBox11.ValidatingType = typeof(int);
             this.maskedTextBox11.Watermark = "Enter some text...";
@@ -176,6 +199,8 @@
             this.Controls.Add(this.maskedTextBox1);
             this.MinimumSize = new System.Drawing.Size(840, 309);
             this.Name = "Features";
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles"))));
+            this.ResponsiveProfiles.Add(((Wisej.Base.ResponsiveProfile)(resources.GetObject("$this.ResponsiveProfiles1"))));
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Demos/Editors/Wisej.DemoBrowser.MonthCalendar/Features.Designer.cs
+++ Demos/Editors/Wisej.DemoBrowser.MonthCalendar/Features.Designer.cs
@@ -34,28 +34,27 @@
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Anchor = Wisej.Web.AnchorStyles.None;
             this.monthCalendar1.AutoSize = true;
-            this.monthCalendar1.Location = new System.Drawing.Point(286, 74);
+            this.monthCalendar1.Location = new System.Drawing.Point(47, 91);
             this.monthCalendar1.MonthlyBoldedDates = new System.DateTime[] {
         new System.DateTime(2022, 1, 15, 0, 0, 0, 0)};
             this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.Size = new System.Drawing.Size(299, 327);
+            this.monthCalendar1.Size = new System.Drawing.Size(260, 234);
             this.monthCalendar1.TabIndex = 0;
             this.monthCalendar1.DateChanged += new Wisej.Web.DateRangeEventHandler(this.monthCalendar1_DateChanged);
             // 
             // monthCalendar3
             // 
-            this.monthCalendar3.Anchor = Wisej.Web.AnchorStyles.None;
+            this.monthCalendar3.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Right)));
             this.monthCalendar3.AutoSize = true;
             this.monthCalendar3.CalendarDimensions = new System.Drawing.Size(2, 1);
-            this.monthCalendar3.Location = new System.Drawing.Point(603, 74);
+            this.monthCalendar3.Location = new System.Drawing.Point(343, 91);
             this.monthCalendar3.MaxDate = new System.DateTime(2023, 1, 1, 0, 0, 0, 0);
             this.monthCalendar3.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
             this.monthCalendar3.Name = "monthCalendar3";
             this.monthCalendar3.ShowOtherMonth = false;
             this.monthCalendar3.ShowWeekNumbers = false;
-            this.monthCalendar3.Size = new System.Drawing.Size(534, 325);
+            this.monthCalendar3.Size = new System.Drawing.Size(464, 234);
             this.monthCalendar3.TabIndex = 2;
             this.monthCalendar3.TodayFormat = "MMM dd";
             // 
@@ -65,6 +64,7 @@
             this.Controls.Add(this.monthCalendar1);
             this.MinimumSize = new System.Drawing.Size(854, 328);
             this.Name = "Features";
+            this.Size = new System.Drawing.Size(878, 474);
             this.ResumeLayout(false);
             this.PerformLayout();
 
