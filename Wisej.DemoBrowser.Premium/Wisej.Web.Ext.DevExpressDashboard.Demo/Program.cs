2a135e9752e951ca25181cd8022bcd14f766c1fa
JulieBodette <jr3python@gmail.com>
2023-09-01 15:18:34 -0400

    Added creation and deletion of temp folder
    creates a temporary folder when a new user / session is created and deletes it when the user leaves

--- Wisej.DemoBrowser.Premium/Wisej.Web.Ext.DevExpressDashboard.Demo/Program.cs
+++ Wisej.DemoBrowser.Premium/Wisej.Web.Ext.DevExpressDashboard.Demo/Program.cs
@@ -11,6 +11,17 @@ namespace Wisej.Web.Ext.DevExpressDashboard.Demo
 		static void Main()
 		{
 			Application.MainPage = new MainView();
+
+			//create temp folder (will be deleted when user closes application)
+			System.IO.Directory.CreateDirectory("./Temp");
+
+			//delete temp folder when user closes application
+			Application.ApplicationExit += new System.EventHandler(DeleteTempFolder);
+		}
+
+		static void DeleteTempFolder(object sender, System.EventArgs e)
+		{
+			System.IO.Directory.Delete("./Temp");
 		}
 
 		//
