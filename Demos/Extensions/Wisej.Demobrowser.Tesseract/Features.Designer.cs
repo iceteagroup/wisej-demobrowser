7c49440d4430fde1d12134aa589548659c029cbd
JulieBodette <jr3python@gmail.com>
2024-02-01 11:32:55 -0500

    worked on Tesseract sample

--- Demos/Extensions/Wisej.Demobrowser.Tesseract/Features.Designer.cs
+++ Demos/Extensions/Wisej.Demobrowser.Tesseract/Features.Designer.cs
@@ -28,150 +28,184 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Features));
+			this.pictureBox1 = new Wisej.Web.PictureBox();
+			this.buttonScan1 = new Wisej.Web.Button();
+			this.button2 = new Wisej.Web.Button();
+			this.textBox1 = new Wisej.Web.TextBox();
+			this.label2 = new Wisej.Web.Label();
+			this.comboBox1 = new Wisej.Web.ComboBox();
+			this.tabControl1 = new Wisej.Web.TabControl();
+			this.tabPage1 = new Wisej.Web.TabPage();
+			this.tabPage2 = new Wisej.Web.TabPage();
+			this.tabPage3 = new Wisej.Web.TabPage();
+			this.upload1 = new Wisej.Web.Upload();
+			this.tabPage4 = new Wisej.Web.TabPage();
 			this.camera1 = new Wisej.Web.Ext.Camera.Camera();
-			this.checkBoxVideo = new Wisej.Web.CheckBox();
-			this.checkBoxAudio = new Wisej.Web.CheckBox();
-			this.comboBoxFacingMode = new Wisej.Web.ComboBox();
-			this.comboBoxObjectFit = new Wisej.Web.ComboBox();
-			this.panel1 = new Wisej.Web.Panel();
-			this.flowLayoutPanel1 = new Wisej.Web.FlowLayoutPanel();
-			this.label1 = new Wisej.Web.Label();
-			this.panel1.SuspendLayout();
-			this.flowLayoutPanel1.SuspendLayout();
+			this.tesseract1 = new Wisej.Ext.Tesseract.Tesseract(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Left | Wisej.Web.AnchorStyles.Right)));
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(23, 25);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(654, 313);
+			this.pictureBox1.SizeMode = Wisej.Web.PictureBoxSizeMode.Zoom;
+			// 
+			// buttonScan1
+			// 
+			this.buttonScan1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Left | Wisej.Web.AnchorStyles.Right)));
+			this.buttonScan1.Location = new System.Drawing.Point(722, 125);
+			this.buttonScan1.Name = "buttonScan1";
+			this.buttonScan1.Size = new System.Drawing.Size(256, 37);
+			this.buttonScan1.TabIndex = 0;
+			this.buttonScan1.Text = "Scan Picturebox Image for Text";
+			this.buttonScan1.Click += new System.EventHandler(this.buttonScan1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(22, 200);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(255, 37);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Scan Image from url";
+			// 
+			// textBox1
+			// 
+			this.textBox1.LabelText = "Image Url:";
+			this.textBox1.Location = new System.Drawing.Point(22, 86);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(255, 53);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(417, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 18);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "label2";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Left | Wisej.Web.AnchorStyles.Right)));
+			this.comboBox1.Items.AddRange(new object[] {
+            "frogText.png",
+            "webDevelopment.jpg",
+            "wisej-logo.png"});
+			this.comboBox1.LabelText = "Select image...";
+			this.comboBox1.Location = new System.Drawing.Point(722, 42);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(237, 53);
+			this.comboBox1.TabIndex = 8;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Left | Wisej.Web.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Location = new System.Drawing.Point(20, 31);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.PageInsets = new Wisej.Web.Padding(1, 40, 1, 1);
+			this.tabControl1.Size = new System.Drawing.Size(1103, 405);
+			this.tabControl1.TabIndex = 7;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.comboBox1);
+			this.tabPage1.Controls.Add(this.buttonScan1);
+			this.tabPage1.Controls.Add(this.pictureBox1);
+			this.tabPage1.Location = new System.Drawing.Point(1, 40);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Size = new System.Drawing.Size(1101, 364);
+			this.tabPage1.Text = "Picturebox Image";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.label2);
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.textBox1);
+			this.tabPage2.Location = new System.Drawing.Point(1, 40);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Size = new System.Drawing.Size(1101, 364);
+			this.tabPage2.Text = "Image Url";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.upload1);
+			this.tabPage3.Location = new System.Drawing.Point(1, 40);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(1101, 364);
+			this.tabPage3.Text = "Uploaded Image";
+			// 
+			// upload1
+			// 
+			this.upload1.Location = new System.Drawing.Point(72, 65);
+			this.upload1.Name = "upload1";
+			this.upload1.Size = new System.Drawing.Size(296, 192);
+			this.upload1.TabIndex = 0;
+			this.upload1.Text = "upload1";
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.camera1);
+			this.tabPage4.Location = new System.Drawing.Point(1, 40);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Size = new System.Drawing.Size(1101, 364);
+			this.tabPage4.Text = "Image From Camera";
 			// 
 			// camera1
 			// 
-			this.camera1.Anchor = Wisej.Web.AnchorStyles.Top;
-			this.flowLayoutPanel1.SetFillWeight(this.camera1, 2);
-			this.camera1.Location = new System.Drawing.Point(438, 3);
-			this.camera1.MaximumSize = new System.Drawing.Size(700, 0);
-			this.camera1.MinimumSize = new System.Drawing.Size(200, 0);
+			this.camera1.Location = new System.Drawing.Point(30, 39);
 			this.camera1.Name = "camera1";
-			this.camera1.Size = new System.Drawing.Size(700, 367);
+			this.camera1.Size = new System.Drawing.Size(343, 287);
 			this.camera1.TabIndex = 0;
 			this.camera1.Text = "camera1";
-			// 
-			// checkBoxVideo
-			// 
-			this.checkBoxVideo.Anchor = Wisej.Web.AnchorStyles.None;
-			this.checkBoxVideo.Appearance = Wisej.Web.Appearance.Switch;
-			this.checkBoxVideo.AutoSize = false;
-			this.checkBoxVideo.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-			this.checkBoxVideo.CheckState = Wisej.Web.CheckState.Checked;
-			this.checkBoxVideo.Location = new System.Drawing.Point(114, 92);
-			this.checkBoxVideo.Name = "checkBoxVideo";
-			this.checkBoxVideo.Size = new System.Drawing.Size(200, 35);
-			this.checkBoxVideo.TabIndex = 1;
-			this.checkBoxVideo.Text = "Video";
-			this.checkBoxVideo.CheckedChanged += new System.EventHandler(this.checkBoxVideo_CheckedChanged);
-			// 
-			// checkBoxAudio
-			// 
-			this.checkBoxAudio.Anchor = Wisej.Web.AnchorStyles.None;
-			this.checkBoxAudio.Appearance = Wisej.Web.Appearance.Switch;
-			this.checkBoxAudio.AutoSize = false;
-			this.checkBoxAudio.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-			this.checkBoxAudio.Location = new System.Drawing.Point(114, 12);
-			this.checkBoxAudio.Name = "checkBoxAudio";
-			this.checkBoxAudio.Size = new System.Drawing.Size(200, 35);
-			this.checkBoxAudio.TabIndex = 2;
-			this.checkBoxAudio.Text = "Audio";
-			this.checkBoxAudio.CheckedChanged += new System.EventHandler(this.checkBoxAudio_CheckedChanged);
-			// 
-			// comboBoxFacingMode
-			// 
-			this.comboBoxFacingMode.Anchor = Wisej.Web.AnchorStyles.None;
-			this.comboBoxFacingMode.AutoSize = false;
-			this.comboBoxFacingMode.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList;
-			this.comboBoxFacingMode.Items.AddRange(new object[] {
-            "User",
-            "Environment",
-            "Left",
-            "Right"});
-			this.comboBoxFacingMode.LabelText = "Facing Mode";
-			this.comboBoxFacingMode.Location = new System.Drawing.Point(112, 172);
-			this.comboBoxFacingMode.Name = "comboBoxFacingMode";
-			this.comboBoxFacingMode.Size = new System.Drawing.Size(200, 60);
-			this.comboBoxFacingMode.TabIndex = 3;
-			this.comboBoxFacingMode.Text = "User";
-			this.comboBoxFacingMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxFacingMode_SelectedIndexChanged);
-			// 
-			// comboBoxObjectFit
-			// 
-			this.comboBoxObjectFit.Anchor = Wisej.Web.AnchorStyles.None;
-			this.comboBoxObjectFit.AutoSize = false;
-			this.comboBoxObjectFit.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList;
-			this.comboBoxObjectFit.Items.AddRange(new object[] {
-            "Fill",
-            "Contain",
-            "Cover",
-            "ScaleDown",
-            "None"});
-			this.comboBoxObjectFit.LabelText = "Object Fit";
-			this.comboBoxObjectFit.Location = new System.Drawing.Point(112, 277);
-			this.comboBoxObjectFit.Name = "comboBoxObjectFit";
-			this.comboBoxObjectFit.Size = new System.Drawing.Size(200, 60);
-			this.comboBoxObjectFit.TabIndex = 4;
-			this.comboBoxObjectFit.Text = "Contain";
-			this.comboBoxObjectFit.SelectedIndexChanged += new System.EventHandler(this.comboBoxObjectFit_SelectedIndexChanged);
-			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.comboBoxObjectFit);
-			this.panel1.Controls.Add(this.comboBoxFacingMode);
-			this.panel1.Controls.Add(this.checkBoxAudio);
-			this.panel1.Controls.Add(this.checkBoxVideo);
-			this.flowLayoutPanel1.SetFillWeight(this.panel1, 1);
-			this.panel1.Location = new System.Drawing.Point(3, 3);
-			this.panel1.MaximumSize = new System.Drawing.Size(700, 0);
-			this.panel1.MinimumSize = new System.Drawing.Size(300, 0);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(429, 367);
-			this.panel1.TabIndex = 5;
-			// 
-			// flowLayoutPanel1
-			// 
-			this.flowLayoutPanel1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Left | Wisej.Web.AnchorStyles.Right)));
-			this.flowLayoutPanel1.AutoSize = true;
-			this.flowLayoutPanel1.Controls.Add(this.panel1);
-			this.flowLayoutPanel1.Controls.Add(this.camera1);
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(141, 49);
-			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(1141, 376);
-			this.flowLayoutPanel1.TabIndex = 6;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(512, 25);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(176, 18);
-			this.label1.TabIndex = 7;
-			this.label1.Text = "Tesseract demo coming soon!";
 			// 
 			// Features
 			// 
 			this.AutoScroll = true;
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.tabControl1);
 			this.Name = "Features";
-			this.panel1.ResumeLayout(false);
-			this.flowLayoutPanel1.ResumeLayout(false);
+			this.Size = new System.Drawing.Size(1152, 474);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage4.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
-
+		private Web.PictureBox pictureBox1;
+		private Web.TextBox textBox1;
+		private Web.Button button2;
+		private Web.Button buttonScan1;
+		private Web.ComboBox comboBox1;
+		private Web.Label label2;
+		private Web.TabControl tabControl1;
+		private Web.TabPage tabPage1;
+		private Web.TabPage tabPage2;
+		private Web.TabPage tabPage3;
+		private Web.TabPage tabPage4;
+		private Web.Upload upload1;
 		private Web.Ext.Camera.Camera camera1;
-		private Web.CheckBox checkBoxVideo;
-		private Web.CheckBox checkBoxAudio;
-		private Web.ComboBox comboBoxFacingMode;
-		private Web.ComboBox comboBoxObjectFit;
-        private Web.Panel panel1;
-        private Web.FlowLayoutPanel flowLayoutPanel1;
-		private Web.Label label1;
+		private Ext.Tesseract.Tesseract tesseract1;
 	}
 }
--- Demos/Extensions/Wisej.Demobrowser.Tesseract/Features.cs
+++ Demos/Extensions/Wisej.Demobrowser.Tesseract/Features.cs
@@ -12,26 +12,9 @@ namespace Wisej.DemoBrowser.Camera
 			InitializeComponent();
 		}
 
-		private void checkBoxAudio_CheckedChanged(object sender, EventArgs e)
+		private void buttonScan1_Click(object sender, EventArgs e)
 		{
-			this.camera1.Audio = this.checkBoxAudio.Checked;
-		}
 
-		private void checkBoxVideo_CheckedChanged(object sender, EventArgs e)
-		{
-			this.camera1.Video = this.checkBoxVideo.Checked;
-		}
-
-		private void comboBoxFacingMode_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			var mode = (string)this.comboBoxFacingMode.SelectedItem;
-			this.camera1.FacingMode = (VideoFacingMode)Enum.Parse(typeof(VideoFacingMode), mode);
-		}
-
-		private void comboBoxObjectFit_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			var fit = (string)this.comboBoxObjectFit.SelectedItem;
-			this.camera1.ObjectFit = (ObjectFit)Enum.Parse(typeof(ObjectFit), fit);
 		}
 	}
 }
